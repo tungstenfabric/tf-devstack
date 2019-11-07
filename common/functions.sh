9515bea41215039dde8de80f70bac7b621b23dc0
Andrey Pavlov <andrey.mp@gmail.com>
2019-11-07 17:33:29 +0300

    fix docker create cmd run
    Issue-ID: TFB-1497
    Signed-off-by: Andrey Pavlov <andrey.mp@gmail.com>
    Change-Id: I168330f9c2a6b70ab88f0bf726f6244d58a51db3

--- common/functions.sh
+++ common/functions.sh
@@ -44,7 +44,7 @@ function fetch_deployer() {
   [ "$(whoami)" != "root" ] && sudo_cmd="sudo"
   $sudo_cmd rm -rf "$WORKSPACE/$DEPLOYER_DIR"
   ensure_insecure_registry_set $CONTAINER_REGISTRY
-  $sudo_cmd docker create --name $DEPLOYER_IMAGE $CONTAINER_REGISTRY/$DEPLOYER_IMAGE:$CONTRAIL_CONTAINER_TAG
+  $sudo_cmd docker create --name $DEPLOYER_IMAGE --entrypoint /bin/true $CONTAINER_REGISTRY/$DEPLOYER_IMAGE:$CONTRAIL_CONTAINER_TAG
   $sudo_cmd docker cp $DEPLOYER_IMAGE:$DEPLOYER_DIR $WORKSPACE
   $sudo_cmd docker rm -fv $DEPLOYER_IMAGE
   $sudo_cmd chown -R $USER "$WORKSPACE/$DEPLOYER_DIR"
