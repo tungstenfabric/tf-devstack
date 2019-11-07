650ea0dc036e3dd4ba761e1e17587f107ea2ce8a
alexandrelevine <alevine@progmaticlab.com>
2019-11-07 20:20:53 +0300

    Fixed absence of sourcing functions.sh
    Signed-off-by: alexandrelevine <alevine@progmaticlab.com>
    Issue-ID: TFB-1498
    Change-Id: Iea6d683de0794b49edbf35b9c068d689f489f3cc

--- helm/deploy_tf_helm.sh
+++ helm/deploy_tf_helm.sh
@@ -3,6 +3,7 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 source "$my_dir/../common/common.sh"
+source "$my_dir/../common/functions.sh"
 
 TF_HELM_FOLDER=${TF_HELM_FOLDER:-tf-helm-deployer}
 TF_HELM_URL=${TF_HELM_URL:-https://github.com/tungstenfabric/tf-helm-deployer}
