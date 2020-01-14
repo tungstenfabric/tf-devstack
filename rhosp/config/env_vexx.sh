384c891275f2560e4571d7ba39cef118913e7f79
Gleb Galkin <gleb108@gmail.com>
2020-01-14 16:21:18 +0300

    Preparing for CICD pipeline
    Issue-ID: TFB-1520
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: Ia3d5e3390cf4378c9e5bb4c8f304007c0c8d7f4a

--- rhosp/config/env_vexx.sh
+++ rhosp/config/env_vexx.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-
-
 #Password for user stack
 export IPMI_PASSWORD="qwe123QWE"
 
 export SSH_USER='cloud-user'
 export SSH_PUBLIC_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqFitjdvlQlaVJ8oBTkm3Qt48XCNh8ikbYN38WofGJk5oGXtC35H9eBBJ8giv42Lw4JXzBmVSEceMPEmTnIM3JPEhl/uNgn+Y+0e+pInq6bt3+DjjZxLvhun7G3LP8RgwYMvMWUkNEHnwLaCKipjfzrPkp0uD/1ZQVjY799gSyDX2PylneiLNSSWQxvOwNe8dzLyVTxlS2jFzNmMX5I5a9/z2Dw9PTB8FdFQbAKc7ZqaiYBrp3kaTcBlQh2pRpKEGGhosKhp4DPHoQV/f3myfl3sAZNGfpbFLzBxLyY/nHIJ3w2AsWahxKnxdGSxhmmp5KJ6zl4+OhJdNZEb2glK2l gleb@dell'
-
-#SSH public key for user
-#export ssh_private_key="/home/jenkins/.ssh/id_rsa"
-#export ssh_public_key="/home/jenkins/.ssh/id_rsa.pub"
 
 #IP, subnets
 #export mgmt_subnet="192.168.10"
@@ -31,8 +25,3 @@ export SSH_PUBLIC_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqFitjdvlQlaVJ8oBTk
 #export overcloud_compute_prov_ip="${prov_subnet}.9"
 #export overcloud_ctrlcont_prov_ip="${prov_subnet}.48"
 
-
-export CONTRAIL_VERSION="5.1"
-
-#RHEL variables
-export RHEL_POOL_ID=8a85f99c68b939320168c7f5b5b2461c
--- rhosp/providers/vexx/create_env.sh
+++ rhosp/providers/vexx/create_env.sh
@@ -1,39 +1,70 @@
 #!/bin/bash
 
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
 if [[ -z ${OS_USERNAME+x}  && -z ${OS_PASSWORD+x} && -z ${OS_PROJECT_ID+x} ]]; then
    echo "Please export variables from VEXX openrc file first";
    echo Exiting
    exit 1
 fi
 
-#Using existing rhosp13-mgmt network
-#openstack network create rhosp13-mgmt
-#openstack subnet create rhosp13-test --network rhosp13-test --subnet-range 192.168.10.0/24
-#openstack router add subnet router1 rhosp13-mgmt
 
-openstack network create rhosp13-prov
-openstack subnet create rhosp13-prov --network rhosp13-prov --subnet-range 192.168.12.0/24 --allocation-pool start=192.168.12.50,end=192.168.12.70 --gateway none
+flavor=${vm_type:-'v2-standard-4'}
+key_name=${key_name:-'worker'}
+management_network_name="rhosp13-mgmt"
+provider_network_base_name="rhosp13-prov"
+
+#Using existing rhosp13-mgmt network
+#openstack network create ${management_network_name}
+#openstack subnet create ${management_network_name} --network ${management_network_name} --subnet-range 192.168.10.0/24
+#openstack router add subnet router1 ${management_network_name}
+
+prov_subnet_base_prefix='192.168'
+
+for i in $(seq 12 50); do
+  cidr="${prov_subnet_base_prefix}.$i.0/24"
+  echo Checking $cidr
+  subnet_check=$(openstack subnet list --subnet-range ${cidr} -f value -c ID)
+  if [[ "$subnet_check" == "" ]]; then
+     #Unique id for parallel deployments
+     rhosp_id=$i
+     provider_network_name="${provider_network_base_name}-$rhosp_id"
+     _start=$"${prov_subnet_base_prefix}.$rhosp_id.50"
+     _end=$"${prov_subnet_base_prefix}.$rhosp_id.70"
+     echo subnet range $cidr is available. Creating
+     openstack network create ${provider_network_name}
+     openstack subnet create ${provider_network_name} --network ${provider_network_name} --subnet-range ${cidr} --allocation-pool start=${_start},end=${_end} --gateway none
+     break;
+  fi
+done
+
+undercloud_instance="rhosp13-undercloud-${rhosp_id}"
+overcloud_cont_instance="rhosp13-overcloud-cont-${rhosp_id}"
+overcloud_compute_instance="rhosp13-overcloud-compute-${rhosp_id}"
+overcloud_ctrlcont_instance="rhosp13-overcloud-ctrlcont-${rhosp_id}"
 
 #Get latest rhel image
 image_name=$(openstack image list  -f value -c Name | grep template-rhel-7 | tail -1)
 image_id=$(openstack image list --name ${image_name} -f value -c ID)
 
-nova boot --flavor v2-standard-4 \
+
+nova boot --flavor ${flavor} \
           --security-groups allow_all \
-          --key-name=gleb \
-          --nic net-name=rhosp13-mgmt \
-          --nic net-name=rhosp13-prov \
+          --key-name=${key_name} \
+          --nic net-name=${management_network_name} \
+          --nic net-name=${provider_network_name} \
           --block-device source=image,id=${image_id},dest=volume,shutdown=remove,size=40,bootindex=0 \
           --poll \
-          rhosp13-undercloud
+          ${undercloud_instance}
 
 #Disabling port security on prov-network interface
-openstack port list --server rhosp13-undercloud --network rhosp13-prov
-port_id=$(openstack port list --server rhosp13-undercloud --network rhosp13-prov -f value -c id)
+openstack port list --server ${undercloud_instance} --network ${provider_network_name}
+port_id=$(openstack port list --server ${undercloud_instance} --network ${provider_network_name} -f value -c id)
 openstack port set --no-security-group --disable-port-security $port_id
 
 #Assigning floating ip
-port_id=$(openstack port list --server rhosp13-undercloud --network rhosp13-mgmt -f value -c id)
+port_id=$(openstack port list --server ${undercloud_instance} --network ${management_network_name} -f value -c id)
 floating_ip_check=$(openstack floating ip list --port ${port_id} -f value -c ID)
 
 i="0"
@@ -42,7 +73,7 @@ while [[ "$floating_ip_check" == "" ]]; do
   i=$(( $i+1 ))
   echo Try $i out of $limit. floating_ip_check="$floating_ip_check"
   floating_ip=$(openstack floating ip create 0048fce6-c715-4106-a810-473620326cb0 -f value -c name)
-  openstack server add floating ip rhosp13-undercloud $floating_ip
+  openstack server add floating ip ${undercloud_instance} $floating_ip
   floating_ip_check=$(openstack floating ip list --port ${port_id} -f value -c ID)
   sleep 3
   if (( $i > $limit)); then
@@ -50,47 +81,55 @@ while [[ "$floating_ip_check" == "" ]]; do
   fi
 done
 
-openstack server show rhosp13-undercloud
+openstack server show ${undercloud_instance}
 
 floating_ip=$(openstack floating ip list --port ${port_id} -f value -c "Floating IP Address")
 
+#Creating cleanup script
+export floating_ip undercloud_instance overcloud_cont_instance overcloud_compute_instance overcloud_ctrlcont_instance provider_network_name
+cat "${my_dir}/cleanup.sh.template" | envsubst > cleanup.sh
+chmod 755 cleanup.sh
 
 #Creating overcloud nodes and disabling port security
-for instance_name in rhosp13-overcloud-cont rhosp13-overcloud-compute rhosp13-overcloud-ctrlcont; do
+for instance_name in ${overcloud_cont_instance} ${overcloud_compute_instance} ${overcloud_ctrlcont_instance}; do
     nova boot --flavor v2-standard-4 --security-groups allow_all --key-name=gleb \
-              --nic net-name=rhosp13-prov \
+              --nic net-name=${provider_network_name} \
               --block-device source=image,id=${image_id},dest=volume,shutdown=remove,size=30,bootindex=0 \
               --poll $instance_name
-    port_id=$(openstack port list --server $instance_name --network rhosp13-prov -f value -c id)
+    port_id=$(openstack port list --server $instance_name --network ${provider_network_name} -f value -c id)
     openstack port set --no-security-group --disable-port-security $port_id
 done
 
 
-mgmt_subnet=$(openstack subnet list --name rhosp13-mgmt -f value -c Subnet | egrep -o '([0-9]{1,3}\.){2}[0-9]{1,3}')
-mgmt_subnet_gateway_ip=$(openstack subnet show rhosp13-mgmt -f value -c gateway_ip)
-prov_subnet=$(openstack subnet list --name rhosp13-prov -f value -c Subnet | egrep -o '([0-9]{1,3}\.){2}[0-9]{1,3}')
-undercloud_ip_addresses=$(openstack server show rhosp13-undercloud -f value -c addresses)
+mgmt_subnet=$(openstack subnet list --name ${management_network_name} -f value -c Subnet | egrep -o '([0-9]{1,3}\.){2}[0-9]{1,3}')
+mgmt_subnet_gateway_ip=$(openstack subnet show ${management_network_name} -f value -c gateway_ip)
+prov_subnet=$(openstack subnet list --name ${provider_network_name} -f value -c Subnet | egrep -o '([0-9]{1,3}\.){2}[0-9]{1,3}')
+undercloud_ip_addresses=$(openstack server show ${undercloud_instance} -f value -c addresses)
 undercloud_mgmt_ip=$(echo ${undercloud_ip_addresses} | egrep -o 'rhosp13-mgmt=.[0-9.]*' | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
-undercloud_prov_ip=$(echo ${undercloud_ip_addresses} | egrep -o 'rhosp13-prov=.[0-9.]*' | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
+undercloud_prov_ip=$(echo ${undercloud_ip_addresses} | egrep -o 'rhosp13-prov-[0-9]{2}=.[0-9.]*' | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
 
-overcloud_cont_ip=$(openstack server show rhosp13-overcloud-cont -f value -c addresses | cut -d '=' -f 2)
-overcloud_compute_ip=$(openstack server show rhosp13-overcloud-compute -f value -c addresses | cut -d '=' -f 2)
-overcloud_ctrlcont_ip=$(openstack server show rhosp13-overcloud-ctrlcont -f value -c addresses | cut -d '=' -f 2)
+overcloud_cont_ip=$(openstack server show ${overcloud_cont_instance} -f value -c addresses | cut -d '=' -f 2)
+overcloud_compute_ip=$(openstack server show ${overcloud_compute_instance} -f value -c addresses | cut -d '=' -f 2)
+overcloud_ctrlcont_ip=$(openstack server show ${overcloud_ctrlcont_instance} -f value -c addresses | cut -d '=' -f 2)
 
+
+
+vexxrc="${my_dir}/../../config/env_vexx.sh"
 
 echo Undercloud is available by floating ip: $floating_ip
 echo
-echo Please put following lines to your environment file tf-devstack/config/env_vexx.sh
+echo file tf-devstack/config/env_vexx.sh was updated
 echo ==================================================================================
-echo export mgmt_subnet=\""${mgmt_subnet}"\"
-echo export prov_subnet=\""${prov_subnet}"\"
-echo export mgmt_gateway=\""${mgmt_subnet_gateway_ip}"\"
-echo export mgmt_ip=\""${undercloud_mgmt_ip}"\"
-echo export prov_ip=\""${undercloud_prov_ip}"\"
-echo export fixed_vip=\""${prov_subnet}.200"\"
-echo export fixed_controller_ip=\""${prov_subnet}.211"\"
-echo export overcloud_cont_prov_ip=\""${overcloud_cont_ip}"\"
-echo export overcloud_compute_prov_ip=\""${overcloud_compute_ip}"\"
-echo export overcloud_ctrlcont_prov_ip=\""${overcloud_ctrlcont_ip}"\"
 
+echo export mgmt_subnet=\""${mgmt_subnet}"\" >> $vexxrc
+echo export prov_subnet=\""${prov_subnet}"\" >> $vexxrc
+echo export mgmt_gateway=\""${mgmt_subnet_gateway_ip}"\" >> $vexxrc
+echo export mgmt_ip=\""${undercloud_mgmt_ip}"\" >> $vexxrc
+echo export prov_ip=\""${undercloud_prov_ip}"\" >> $vexxrc
+echo export fixed_vip=\""${prov_subnet}.200"\" >> $vexxrc
+echo export fixed_controller_ip=\""${prov_subnet}.211"\" >> $vexxrc
+echo export overcloud_cont_prov_ip=\""${overcloud_cont_ip}"\" >> $vexxrc
+echo export overcloud_compute_prov_ip=\""${overcloud_compute_ip}"\" >> $vexxrc
+echo export overcloud_ctrlcont_prov_ip=\""${overcloud_ctrlcont_ip}"\" >> $vexxrc
 
+cat $vexxrc
