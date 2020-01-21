e82c14daee1083f169a43b8b6cb23e4cb313c7e2
Gleb Galkin <gleb108@gmail.com>
2020-01-21 14:32:25 +0300

    rhosp log collection
    Issue-ID: TFB-1520
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I7146dcfacd76771cc56c5cf31f1bbd28c809aef8

--- rhosp/run.sh
+++ rhosp/run.sh
@@ -82,7 +82,7 @@ function undercloud() {
 function overcloud() {
   cd $my_dir
   for ip in $overcloud_cont_prov_ip $overcloud_compute_prov_ip $overcloud_ctrlcont_prov_ip; do
-     scp $ssh_opts ~/rhosp-environment.sh providers/common/* overcloud/03_setup_predeployed_nodes.sh $SSH_USER@$ip:
+     scp $ssh_opts ~/rhosp-environment.sh ../common/collect_logs.sh providers/common/* overcloud/03_setup_predeployed_nodes.sh $SSH_USER@$ip:
      #ssh $ssh_opts $SSH_USER@$ip ./add_user_stack.sh
      #scp $ssh_opts ~/rhosp-environment.sh providers/common/* overcloud/03_setup_predeployed_nodes.sh stack@$ip:
      ssh $ssh_opts $SSH_USER@$ip sudo ./03_setup_predeployed_nodes.sh &
@@ -96,6 +96,39 @@ function tf() {
    sudo ./overcloud/05_prepare_containers.sh
    ./overcloud/06_deploy_overcloud.sh
 }
+
+function logs() {
+    local errexit_state=$(echo $SHELLOPTS| grep errexit | wc -l)
+    set +e
+
+    #Collecting undercloud logs
+    create_log_dir
+    collect_system_stats
+    hostname=$(hostname -s)
+    mkdir ${TF_LOG_DIR}/${hostname}
+    mv ${TF_LOG_DIR}/* ${TF_LOG_DIR}/${hostname}/
+    cp $WORKSPACE/tf-devstack-values.yaml ${TF_LOG_DIR}/
+
+    #Collecting overcloud logs
+    for ip in $overcloud_cont_prov_ip $overcloud_compute_prov_ip $overcloud_ctrlcont_prov_ip; do
+        ssh $ssh_opts $SSH_USER@$ip TF_LOG_DIR="/home/${SSH_USER}/logs" ./collect_logs.sh create_log_dir
+        ssh $ssh_opts $SSH_USER@$ip TF_LOG_DIR="/home/${SSH_USER}/logs" ./collect_logs.sh collect_docker_logs
+        ssh $ssh_opts $SSH_USER@$ip TF_LOG_DIR="/home/${SSH_USER}/logs" ./collect_logs.sh collect_system_stats
+        ssh $ssh_opts $SSH_USER@$ip TF_LOG_DIR="/home/${SSH_USER}/logs" ./collect_logs.sh collect_contrail_logs
+        hostname=$(ssh $ssh_opts $SSH_USER@$ip hostname -s)
+        mkdir ${TF_LOG_DIR}/${hostname}
+        scp -r $ssh_opts $SSH_USER@$ip:logs/* ${TF_LOG_DIR}/${hostname}/ 
+    done
+
+    tar -czf ${WORKSPACE}/logs.tgz -C ${TF_LOG_DIR}/.. logs
+    #rm -rf $TF_LOG_DIR
+
+    # Restore errexit state
+    if [[ $errexit_state == 1 ]]; then
+        set -e
+    fi
+}
+
 
 
 run_stages $STAGE
