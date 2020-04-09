8d4cd09661d7a6d44651ddb7c668ce15cbcdf18f
alexey-mr <alexey.morlang@gmail.com>
2020-04-09 16:42:44 +0300

    Create openshift settings on platform step
    Issue-ID: TFB-1568
    Change-Id: I01f7af9961c6686b7797a870a4e126263e5cddf1
    Signed-off-by: alexey-mr <alexey.morlang@gmail.com>

--- openshift/run.sh
+++ openshift/run.sh
@@ -33,13 +33,6 @@ deployer_dir=${WORKSPACE}/tf-openshift-ansible-src
 INVENTORY_FILE=${INVENTORY_FILE:-"$deployer_dir/inventory/hosts.aio.contrail"}
 
 settings_file=${WORKSPACE}/tf_openhift_settings
-cat <<EOF > $settings_file
-[all:vars]
-ansible_become=true
-contrail_analyticsdb_jvm_extra_opts="-Xms2g -Xmx4g"
-contrail_configdb_jvm_extra_opts="-Xms1g -Xmx2g"
-
-EOF
 
 function is_registry_insecure() {
     local registry=`echo $1 | sed 's|^.*://||' | cut -d '/' -f 1`
@@ -49,23 +42,36 @@ function is_registry_insecure() {
     return 1
 }
 
-openshift_docker_insecure_registries=""
-[ -n "$CONTRAIL_CONTAINER_TAG" ] && echo "contrail_container_tag=\"$CONTRAIL_CONTAINER_TAG\"" >> $settings_file
-[ -n "$CONTAINER_REGISTRY" ] && {
-    echo "contrail_registry=\"$CONTAINER_REGISTRY\"" >> $settings_file
-    is_registry_insecure "$CONTAINER_REGISTRY" && openshift_docker_insecure_registries+=",$CONTAINER_REGISTRY"
-}
+function make_tf_openshift_settings_file() {
+    local settings_file=$1
+    cat <<EOF > $settings_file
+[all:vars]
+ansible_become=true
+contrail_analyticsdb_jvm_extra_opts="-Xms2g -Xmx4g"
+contrail_configdb_jvm_extra_opts="-Xms1g -Xmx2g"
 
-[ -n "$openshift_image_tag" ] && echo "openshift_image_tag=\"$openshift_image_tag\"" >> $settings_file
-[ -n "$RHEL_OPENSHIFT_REGISTRY" ] && {
-    echo "oreg_url=\"$RHEL_OPENSHIFT_REGISTRY/openshift3/ose-\${component}:\${version}\"" >> $settings_file
-    echo "etcd_image=\"${RHEL_OPENSHIFT_REGISTRY}/rhel7/etcd:3.2.22\"" >> $settings_file
-    echo "system_images_registry=\"$RHEL_OPENSHIFT_REGISTRY\"" >> $settings_file
-    is_registry_insecure $RHEL_OPENSHIFT_REGISTRY && openshift_docker_insecure_registries+=",$RHEL_OPENSHIFT_REGISTRY"
-}
-[ -n "$openshift_docker_insecure_registries" ] && echo "openshift_docker_insecure_registries=\"${openshift_docker_insecure_registries#,}\"" >> $settings_file
-[ -n "$RHEL_USER" ] && echo "oreg_auth_user=\"$RHEL_USER\"" >> $settings_file
-[ -n "$RHEL_PASSWORD" ] && echo "oreg_auth_password=\"$RHEL_PASSWORD\"" >> $settings_file
+EOF
+
+    openshift_docker_insecure_registries=""
+    [ -n "$CONTRAIL_CONTAINER_TAG" ] && echo "contrail_container_tag=\"$CONTRAIL_CONTAINER_TAG\"" >> $settings_file
+    [ -n "$CONTAINER_REGISTRY" ] && {
+        echo "contrail_registry=\"$CONTAINER_REGISTRY\"" >> $settings_file
+        is_registry_insecure "$CONTAINER_REGISTRY" && openshift_docker_insecure_registries+=",$CONTAINER_REGISTRY"
+    }
+
+    [ -n "$openshift_image_tag" ] && echo "openshift_image_tag=\"$openshift_image_tag\"" >> $settings_file
+    [ -n "$RHEL_OPENSHIFT_REGISTRY" ] && {
+        echo "oreg_url=\"$RHEL_OPENSHIFT_REGISTRY/openshift3/ose-\${component}:\${version}\"" >> $settings_file
+        echo "etcd_image=\"${RHEL_OPENSHIFT_REGISTRY}/rhel7/etcd:3.2.22\"" >> $settings_file
+        echo "system_images_registry=\"$RHEL_OPENSHIFT_REGISTRY\"" >> $settings_file
+        is_registry_insecure $RHEL_OPENSHIFT_REGISTRY && openshift_docker_insecure_registries+=",$RHEL_OPENSHIFT_REGISTRY"
+    }
+    [ -n "$openshift_docker_insecure_registries" ] && echo "openshift_docker_insecure_registries=\"${openshift_docker_insecure_registries#,}\"" >> $settings_file
+    [ -n "$RHEL_USER" ] && echo "oreg_auth_user=\"$RHEL_USER\"" >> $settings_file
+    [ -n "$RHEL_PASSWORD" ] && echo "oreg_auth_password=\"$RHEL_PASSWORD\"" >> $settings_file
+    echo "INFO: config $settings_file created:"
+    cat $settings_file | sed 's/password=.*$/password=*****/g' | sed 's/user=.*$/user=*****/g'
+fi
 
 # stages
 
@@ -118,18 +124,25 @@ function platform() {
     [ ! -f /etc/resolv.conf.org.bkp ] && { 
         sudo cp /etc/resolv.conf /etc/resolv.conf.org.bkp
     }
+    make_tf_openshift_settings_file "$settings_file"
     # deploy pre-requisites
     ansible-playbook -i $settings_file \
         -i inventory/hosts.aio.contrail playbooks/prerequisites.yml
 }
 
 function tf() {
+    [ ! -f "$settings_file" ] && {
+        echo "ERROR: $settings_file doesnt exist. run.sh platform is to be called first"
+        return 1
+    }
+    local res=0
     cd $deployer_dir
     ansible-playbook -i $settings_file \
-        -i inventory/hosts.aio.contrail playbooks/deploy_cluster.yml
+        -i inventory/hosts.aio.contrail playbooks/deploy_cluster.yml || res=1
     # show results
     echo "Contrail Web UI will be available at https://$NODE_IP:8143"
     echo "Use admin/contrail123 to log in"
+    return $res
 }
 
 # This is_active function is called in wait stage defined in common/stages.sh
