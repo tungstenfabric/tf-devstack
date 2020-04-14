e0f8ce70c426e672201a300decde712e3533be95
alexey-mr <alexey.morlang@gmail.com>
2020-04-14 14:22:53 +0300

    Fixed vexx_env
    Issue-ID: TFB-1568
    Signed-off-by: alexey-mr <alexey.morlang@gmail.com>
    Change-Id: Ia65f149e500fe54b8d1ba90be7c5f568da26b4ae

--- rhosp/config/vexx_env.sh
+++ rhosp/config/vexx_env.sh
@@ -1,35 +1,34 @@
 
 
-export IPMI_PASSWORD=${IPMI_PASSWORD:-"qwe123QWE"}
+export IPMI_PASSWORD=${IPMI_PASSWORD}
 
-export SSH_USER=${SSH_USER:-'cloud-user'}
+export SSH_USER=${SSH_USER}
 #export SSH_PUBLIC_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqFitjdvlQlaVJ8oBTkm3Qt48XCNh8ikbYN38WofGJk5oGXtC35H9eBBJ8giv42Lw4JXzBmVSEceMPEmTnIM3JPEhl/uNgn+Y+0e+pInq6bt3+DjjZxLvhun7G3LP8RgwYMvMWUkNEHnwLaCKipjfzrPkp0uD/1ZQVjY799gSyDX2PylneiLNSSWQxvOwNe8dzLyVTxlS2jFzNmMX5I5a9/z2Dw9PTB8FdFQbAKc7ZqaiYBrp3kaTcBlQh2pRpKEGGhosKhp4DPHoQV/f3myfl3sAZNGfpbFLzBxLyY/nHIJ3w2AsWahxKnxdGSxhmmp5KJ6zl4+OhJdNZEb2glK2l jenkins@slave'
 
 #IP, subnets
-export mgmt_subnet=${mgmt_subnet:-"192.168.10"}
-export mgmt_gateway=${mgmt_gateway:-"${mgmt_subnet}.1"}
+export mgmt_subnet=${mgmt_subnet}
+export mgmt_gateway=${mgmt_gateway}
 #Undercloud mgmt ip
-export mgmt_ip=${mgmt_ip:-"${mgmt_subnet}.112"}
+export mgmt_ip=${mgmt_ip}
 
-export prov_subnet=${prov_subnet:-"192.168.12"}
-export prov_gateway=${prov_gateway:-"${prov_subnet}.1"}
+export prov_subnet=${prov_subnet}
+export prov_gateway=${prov_gateway}
 
 #Undercloud prov ip
-export prov_ip=${prov_ip:-"${prov_subnet}.36"}
+export prov_ip=${prov_ip}
 
-export fixed_vip=${fixed_vip:-"${prov_subnet}.200"}
-export fixed_controller_ip=${fixed_controller_ip:-"${prov_subnet}.211"}
+export fixed_vip=${fixed_vip}
+export fixed_controller_ip=${fixed_controller_ip}
 
 #Undecloud
-export undercloud_instance=${undercloud_instance:-"undercloud-${DEPLOY_POSTFIX}"}
-export domain=${domain:-'vexxhost.local'}
+export undercloud_instance=${undercloud_instance}
+export domain=${domain}
 
 #Prov IPs for overcloud nodes
-export overcloud_cont_prov_ip=${overcloud_cont_prov_ip:-"${prov_subnet}.40"}
-export overcloud_compute_prov_ip=${overcloud_compute_prov_ip:-"${prov_subnet}.9"}
-export overcloud_ctrlcont_prov_ip=${overcloud_ctrlcont_prov_ip:-"${prov_subnet}.48"}
+export overcloud_cont_prov_ip=${overcloud_cont_prov_ip}
+export overcloud_compute_prov_ip=${overcloud_compute_prov_ip}
+export overcloud_ctrlcont_prov_ip=${overcloud_ctrlcont_prov_ip}
 
-export overcloud_cont_instance=${overcloud_cont_instance:-"$RHOSP_VERSION-overcloud-cont-${DEPLOY_POSTFIX}"}
-export overcloud_compute_instance=${overcloud_compute_instance:-"$RHOSP_VERSION-overcloud-compute-${DEPLOY_POSTFIX}"}
-export overcloud_ctrlcont_instance=${overcloud_ctrlcont_instance:-"$RHOSP_VERSION-overcloud-ctrlcont-${DEPLOY_POSTFIX}"}
-
+export overcloud_cont_instance=${overcloud_cont_instance}
+export overcloud_compute_instance=${overcloud_compute_instance}
+export overcloud_ctrlcont_instance=${overcloud_ctrlcont_instance}
