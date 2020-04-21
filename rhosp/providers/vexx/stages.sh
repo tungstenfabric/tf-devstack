54c981f243a213c035e3624f0cb5ed6258f53538
alexey-mr <alexey.morlang@gmail.com>
2020-04-21 17:55:06 +0300

    Allow fail by -e flag in rhosp stages
    - prepare ssh keys in create_env for vexx
    to be possible use w/o jenkins
    
    - use custom redis port to avoid conflicts
    with openstack redis in AIO
    
    Change-Id: I0338ff1a6e25e523fa5033e134f93b49d4dd2f5c

--- rhosp/providers/vexx/stages.sh
+++ rhosp/providers/vexx/stages.sh
@@ -11,11 +11,11 @@ function machines() {
 
 function undercloud() {
     cd $my_dir
-    sudo ./undercloud/01_deploy_as_root.sh || return 1
-    ./undercloud/02_deploy_as_stack.sh || return 1
+    sudo ./undercloud/01_deploy_as_root.sh
+    ./undercloud/02_deploy_as_stack.sh
     # No needs to have container registry on undercloud.
     # For now overcloud nodes download them directly from $CONTAINER_REGISTRY
-    sudo -E CONTAINER_REGISTRY="" CONFIGURE_DOCKER_LIVERESTORE=false $my_dir/../common/create_docker_config.sh || return 1
+    sudo -E CONTAINER_REGISTRY="" CONFIGURE_DOCKER_LIVERESTORE=false $my_dir/../common/create_docker_config.sh
     local insecure_registries=$(cat /etc/sysconfig/docker | awk -F '=' '/^INSECURE_REGISTRY=/{print($2)}' | tr -d '"')
     if [ -n "$CONTAINER_REGISTRY" ] && is_registry_insecure $CONTAINER_REGISTRY ; then
        insecure_registries+=" --insecure-registry $CONTAINER_REGISTRY"
@@ -25,7 +25,7 @@ function undercloud() {
     if ! sudo systemctl restart docker ; then
         systemctl status docker.service
         journalctl -xe
-        return 1
+        exit 1
     fi
 }
 
