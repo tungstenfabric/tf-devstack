63a09f3894f787f437f9faf9464f4cdbe62c6c33
alexey-mr <alexey.morlang@gmail.com>
2020-04-22 23:33:18 +0300

    Fix log msg for rhosp
    Change-Id: Ibab43b22b1c27603bf8325254f6980737d03ebae

--- rhosp/providers/vexx/stages.sh
+++ rhosp/providers/vexx/stages.sh
@@ -17,14 +17,14 @@ function undercloud() {
     # For now overcloud nodes download them directly from $CONTAINER_REGISTRY
     sudo -E bash -c "CONTAINER_REGISTRY='' CONFIGURE_DOCKER_LIVERESTORE=false $my_dir/../common/create_docker_config.sh"
     local insecure_registries=$(cat /etc/sysconfig/docker | awk -F '=' '/^INSECURE_REGISTRY=/{print($2)}' | tr -d '"')
-    echo "INFO: current insecure_registries=$insecure_registries CONTAINER_REGISTRY=$CONTAINER_REGISTRY"
+    echo "INFO: current insecure_registries=$insecure_registries"
     if [ -n "$CONTAINER_REGISTRY" ] && is_registry_insecure $CONTAINER_REGISTRY ; then
         echo "INFO: add CONTAINER_REGISTRY=$CONTAINER_REGISTRY to insecure list"
        insecure_registries+=" --insecure-registry $CONTAINER_REGISTRY"
     fi
     sudo sed -i '/^INSECURE_REGISTRY/d' /etc/sysconfig/docker
     echo "INSECURE_REGISTRY=\"$insecure_registries\""  | sudo tee -a /etc/sysconfig/docker
-    echo "INFO: /etc/sysconfig/docker"
+    echo "INFO: restart docker, /etc/sysconfig/docker"
     sudo cat /etc/sysconfig/docker
     if ! sudo systemctl restart docker ; then
         echo "ERROR: sudo systemctl restart docker failed"
