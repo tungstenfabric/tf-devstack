62d26151449781120b1660781089d0b6c4241df4
Gleb Galkin <gleb108@gmail.com>
2020-04-22 12:35:20 +0300

    rhosp bugfix
    Issue-ID: TFB-1520
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I30494591062c64a0ea3e4000cd422949fcf31514

--- rhosp/undercloud/01_deploy_as_root.sh
+++ rhosp/undercloud/01_deploy_as_root.sh
@@ -8,6 +8,17 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+if [[ -z ${SUDO_USER+x} ]]; then
+   echo "Stop. Please run this scripts with sudo"
+   exit 1
+fi
+
+if [ -f /home/$SUDO_USER/rhosp-environment.sh ]; then
+   source /home/$SUDO_USER/rhosp-environment.sh
+else
+   echo "File /home/$SUDO_USER/rhosp-environment.sh"
+   exit
+fi
 
 #Specific part of deployment
 source $my_dir/${RHEL_VERSION}_deploy_as_root.sh
