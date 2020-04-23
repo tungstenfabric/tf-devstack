e299513f8402ff313ba59079d95a0a3e9650fd79
alexey-mr <alexey.morlang@gmail.com>
2020-04-23 13:23:53 +0300

    rhosp added -e to avoid ignoring errors
    Change-Id: I3003272cc6ea3436d2934428a7a79c6e9ce269df

--- rhosp/providers/common/rhel7_provisioning.sh
+++ rhosp/providers/common/rhel7_provisioning.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 
 yum update -y
-yum install -y  ntp wget yum-utils vim iproute jq curl bind-utils bridge-utils net-tools python-heat-agent*
+yum install -y ntp wget yum-utils vim iproute jq curl bind-utils bridge-utils net-tools python-heat-agent*
 
 chkconfig ntpd on
 service ntpd start
@@ -11,4 +11,3 @@ service ntpd start
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 pip install -q virtualenv docker
-
--- rhosp/providers/common/rhel8_provisioning.sh
+++ rhosp/providers/common/rhel8_provisioning.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 dnf update -y
 dnf install -y chrony wget yum-utils vim iproute jq curl bind-utils net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
 
 systemctl start chronyd
-
