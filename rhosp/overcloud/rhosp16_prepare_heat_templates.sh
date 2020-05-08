b9f6081b9a493d25ab844ba373d3f8521383d386
Gleb Galkin <gleb108@gmail.com>
2020-05-08 21:20:47 +0300

    rhosp16 preparing overcloud heat templates
    Issue-ID: TFB-1520
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I7f2b7dfb3a035f8a6aaf669dc8a8fb76444d2c3a

---
+++ rhosp/overcloud/rhosp16_prepare_heat_templates.sh
@@ -0,0 +1,10 @@
+
+#Creating rhsm.yaml from template
+
+
+#inserting repos
+for i in `echo $RHEL_REPOS`; do sed -i "/rhsm_repos:/ a \ \ \ \ \ \ - $i" cat $my_dir/rhsm.yaml.template; done
+#Getting orgID
+export RHEL_ORG_ID=$(sudo subscription-manager identity | grep "org ID" | sed -e 's/^.*: //')
+
+cat $my_dir/rhsm.yaml.template | envsubst > ~/rhsm.yaml
--- rhosp/undercloud/rhel8_deploy_as_root.sh
+++ rhosp/undercloud/rhel8_deploy_as_root.sh
@@ -1,4 +1,4 @@
 
 #RHEL8 undercloud install
 
-dnf install -y python3-tripleoclient
+dnf install -y python3-tripleoclient rhosp-director-images rhosp-director-images-ipa
