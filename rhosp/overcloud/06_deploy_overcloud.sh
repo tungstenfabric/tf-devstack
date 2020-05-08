262d75416b28b510d62cf2600d64da4f4aa88071
alexey-mr <alexey.morlang@gmail.com>
2020-05-08 08:55:14 +0300

    Use -e in deploy overcloud
    - to fail job if deploy failed
    
    Change-Id: I1f6473b20b71f0fbbde6aebad3e54cac0a89cfa9

--- rhosp/overcloud/06_deploy_overcloud.sh
+++ rhosp/overcloud/06_deploy_overcloud.sh
@@ -1,28 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 
-if [ -f ~/stackrc ]; then
-   source ~/stackrc
-else
-   echo "File ~/stackrc not found"
-   exit
-fi
-
-if [ -f ~/rhosp-environment.sh ]; then
-   source ~/rhosp-environment.sh
-else
-   echo "File ~/rhosp-environment.sh not found"
-   exit
-fi
-
-
-cd
+cd ~
+source ./stackrc
+source ./rhosp-environment.sh
 
 if [[ "$USE_PREDEPLOYED_NODES" == false ]]; then
-  openstack overcloud deploy --templates tripleo-heat-templates/ \
+   openstack overcloud deploy --templates tripleo-heat-templates/ \
             --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
             -e environment-rhel-registration.yaml \
             -e tripleo-heat-templates/extraconfig/pre_deploy/rhel-registration/rhel-registration-resource-registry.yaml \
@@ -33,18 +20,18 @@ if [[ "$USE_PREDEPLOYED_NODES" == false ]]; then
             -e contrail-parameters.yaml \
             -e docker_registry.yaml
 else
-  if [[ -z "${overcloud_compute_prov_ip}" ]]; then
-    export OVERCLOUD_ROLES="ContrailAio"
-    export ContrailAio_hosts="${overcloud_cont_prov_ip}"
-  else
-    export OVERCLOUD_ROLES="Controller Compute ContrailController"
-    export Controller_hosts="${overcloud_cont_prov_ip}"
-    export Compute_hosts="${overcloud_compute_prov_ip}"
-    export ContrailController_hosts="${overcloud_ctrlcont_prov_ip}"
-  fi
-  nohup tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh &
-
-  openstack overcloud deploy --templates tripleo-heat-templates/ \
+   if [[ -z "${overcloud_compute_prov_ip}" ]]; then
+      export OVERCLOUD_ROLES="ContrailAio"
+      export ContrailAio_hosts="${overcloud_cont_prov_ip}"
+   else
+      export OVERCLOUD_ROLES="Controller Compute ContrailController"
+      export Controller_hosts="${overcloud_cont_prov_ip}"
+      export Compute_hosts="${overcloud_compute_prov_ip}"
+      export ContrailController_hosts="${overcloud_ctrlcont_prov_ip}"
+   fi
+   nohup tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh &
+   job=$!
+   openstack overcloud deploy --templates tripleo-heat-templates/ \
             --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
             --disable-validations \
             -e environment-rhel-registration.yaml \
@@ -59,4 +46,5 @@ else
             -e contrail-parameters.yaml \
             -e ctlplane-assignments.yaml \
             -e docker_registry.yaml
+   kill $job || true
 fi
