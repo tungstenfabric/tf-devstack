780c5ff4b6a7c7a27b6ac8ecc04b4892b218479b
mikeyasnev <mike.yasnev@gmail.com>
2020-05-27 21:05:48 +0300

    Multinode installation for manifest deployment
    Change-Id: I6b2a2325dce8c525f2e00c7feadf513bcf5e5ae1

--- common/workaround.sh
+++ common/workaround.sh
@@ -8,19 +8,39 @@ my_dir="$(dirname $my_file)"
 # In order to avoid this, before starting kubespray, we need to fix the version of docker-ce-cli.
 # This function is called only when kubespray is installed.
 function workaround_kubesray_docker_cli() {
+ 
   DOCKER_CLI_VERSION_YUM=${DOCKER_CLI_VERSION:="docker-ce-cli-18.09*"}
   DOCKER_REPO=${DOCKER_REPO:="https://download.docker.com/linux/centos/docker-ce.repo"}
 
+  echo "INFO: docker cli workaround started"
+  local nodes="${CONTROLLER_NODES} ${AGENT_NODES}"
+  read -a iparr <<< "$nodes"
+  echo "There are ${#iparr[*]} nodes in the cluster"
+
   if [[ "$DISTRO" == "centos" || "$DISTRO" == "rhel" ]]; then
-    sudo yum install -y yum-utils yum-plugin-versionlock
-    sudo yum-config-manager --add-repo ${DOCKER_REPO}
-    sudo yum versionlock ${DOCKER_CLI_VERSION_YUM}
+    for val in "${iparr[@]}";
+    do
+      local userlink="$(whoami)@"
+      userlink+="$val"
+      echo $userlink
+      echo ""
+    
+      ssh $userlink sudo yum install -y yum-utils yum-plugin-versionlock
+      ssh $userlink sudo yum-config-manager --add-repo ${DOCKER_REPO}
+      ssh $userlink sudo yum versionlock ${DOCKER_CLI_VERSION_YUM}
+      ssh $userlink sudo yum versionlock status
+    
+      echo ""
+    done
   elif [[ "$DISTRO" == "ubuntu" ]]; then
-    cat <<EOF > docker-ce-cli
-Package: docker-ce-cli
-Pin: version 5:18.09*
-Pin-Priority: 1001
-EOF
+     cat <<EOF > docker-ce-cli
+	Package: docker-ce-cli
+	Pin: version 5:18.09*
+	Pin-Priority: 1001
+	EOF
     sudo mv docker-ce-cli /etc/apt/preferences.d/
   fi
+
+  echo "INFO: docker cli workaround completed"
+  
 }
