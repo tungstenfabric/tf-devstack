6c0ddb8072a9095e38e7a6bed66415c0ff563ed9
mikeyasnev <mike.yasnev@gmail.com>
2020-05-27 13:46:01 +0300

    Created multinode installation for manifest deployment
    Change-Id: Ie73e251d83bcc0f30767d69fdd04fd1a17423cd7

--- common/workaround.sh
+++ common/workaround.sh
@@ -8,19 +8,31 @@ my_dir="$(dirname $my_file)"
 # In order to avoid this, before starting kubespray, we need to fix the version of docker-ce-cli.
 # This function is called only when kubespray is installed.
 function workaround_kubesray_docker_cli() {
-  DOCKER_CLI_VERSION_YUM=${DOCKER_CLI_VERSION:="docker-ce-cli-18.09*"}
-  DOCKER_REPO=${DOCKER_REPO:="https://download.docker.com/linux/centos/docker-ce.repo"}
 
-  if [[ "$DISTRO" == "centos" || "$DISTRO" == "rhel" ]]; then
-    sudo yum install -y yum-utils yum-plugin-versionlock
-    sudo yum-config-manager --add-repo ${DOCKER_REPO}
-    sudo yum versionlock ${DOCKER_CLI_VERSION_YUM}
-  elif [[ "$DISTRO" == "ubuntu" ]]; then
-    cat <<EOF > docker-ce-cli
-Package: docker-ce-cli
-Pin: version 5:18.09*
-Pin-Priority: 1001
-EOF
-    sudo mv docker-ce-cli /etc/apt/preferences.d/
-  fi
+  echo "docker cli workaround started"
+  nodes="${CONTROLLER_NODES} ${AGENT_NODES}"
+  nodes=`echo "${nodes}" | awk '$1=$1'`
+  IFS=' '
+  read -a iparr <<< "$nodes"
+  echo "There are ${#iparr[*]} nodes in the cluster"
+
+  for val in "${iparr[@]}";
+  do
+    userlink="$(whoami)@"
+    userlink+="$val"
+    echo $userlink
+    echo ""
+    if [[ "$DISTRO" == "centos" || "$DISTRO" == "rhel" ]]; then
+      ssh $userlink sudo yum install -y yum-utils yum-plugin-versionlock
+      ssh $userlink sudo yum-config-manager --add-repo "https://download.docker.com/linux/centos/docker-ce.repo"
+      ssh $userlink sudo yum versionlock "docker-ce-cli-18.09*"
+      ssh $userlink sudo yum versionlock status
+    elif [[ "$DISTRO" == "ubuntu" ]]; then
+      sudo mv docker-ce-cli /etc/apt/preferences.d/
+    fi
+    echo ""
+  done
+
+  echo "docker cli workaround completed"
+
 }
