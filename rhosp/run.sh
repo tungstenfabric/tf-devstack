57b0844f1d1bf375a16ce3013b4b08385c6b4a59
Gleb Galkin <gleb108@gmail.com>
2020-05-28 12:54:32 +0300

    rhosp fix for creating ~/rhosp-environment.sh
    partial-jira-bug: CEM-11349
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: If860e9e7f63eead844d87998bef6a44e1a9ec228

--- rhosp/run.sh
+++ rhosp/run.sh
@@ -78,27 +78,42 @@ source $my_dir/providers/common/functions.sh
 source $my_dir/providers/${PROVIDER}/stages.sh
 
 
+function expand() {
+  while read -r line; do
+    if [[ "$line" =~ ^export ]]; then     
+        line="${line//\\/\\\\}"
+        line="${line//\"/\\\"}"
+        line="${line//\`/\\\`}"
+        eval echo "\"$line\""
+    else
+        echo $line
+    fi
+  done 
+}   
+
 function prepare_rhosp_env_file() {
     ##### Always creating ~/rhosp-environment.sh #####
     rm -f ~/rhosp-environment.sh
     source $my_dir/config/common.sh
-    cat $my_dir/config/common.sh | envsubst >>~/rhosp-environment.sh || true
+    cat $my_dir/config/common.sh | expand >>~/rhosp-environment.sh || true
     source $my_dir/config/${RHEL_VERSION}_env.sh
-    cat $my_dir/config/${RHEL_VERSION}_env.sh | grep '^export' | envsubst >> ~/rhosp-environment.sh || true
+    cat $my_dir/config/${RHEL_VERSION}_env.sh | grep '^export' | expand >> ~/rhosp-environment.sh || true
     source $my_dir/config/${PROVIDER}_env.sh
-    cat $my_dir/config/${PROVIDER}_env.sh | grep '^export' | envsubst >> ~/rhosp-environment.sh || true
+    cat $my_dir/config/${PROVIDER}_env.sh | grep '^export' | expand >> ~/rhosp-environment.sh || true
     echo "export USE_PREDEPLOYED_NODES=$USE_PREDEPLOYED_NODES" >> ~/rhosp-environment.sh
     echo "export PROVIDER=$PROVIDER" >> ~/rhosp-environment.sh
     echo "export RHOSP_VERSION=$RHOSP_VERSION" >> ~/rhosp-environment.sh
     echo "export OPENSTACK_VERSION=$OPENSTACK_VERSION" >> ~/rhosp-environment.sh
     echo "export RHEL_VERSION=$RHEL_VERSION" >> ~/rhosp-environment.sh
     echo "export ENABLE_RHEL_REGISTRATION=$ENABLE_RHEL_REGISTRATION" >> ~/rhosp-environment.sh
-    echo "export CONTRAIL_CONTAINER_TAG=$CONTRAIL_CONTAINER_TAG" >> ~/rhosp-environment.sh
-    echo "export CONTAINER_REGISTRY=$CONTAINER_REGISTRY" >> ~/rhosp-environment.sh
+    echo "export CONTRAIL_CONTAINER_TAG=\"$CONTRAIL_CONTAINER_TAG\"" >> ~/rhosp-environment.sh
+    echo "export CONTAINER_REGISTRY=\"$CONTAINER_REGISTRY\"" >> ~/rhosp-environment.sh
     echo "export IPMI_PASSWORD=\"$IPMI_PASSWORD\"" >> ~/rhosp-environment.sh
     #Removing duplicate lines
-    awk '!a[$0]++' ~/rhosp-environment.sh
+    awk '!a[$0]++' ~/rhosp-environment.sh >/tmp/rhosp-environment.sh
+    cat /tmp/rhosp-environment.sh > ~/rhosp-environment.sh
 }
+
 
 #TODO move inside stage to allow overwrite values by dev-env
 prepare_rhosp_env_file
