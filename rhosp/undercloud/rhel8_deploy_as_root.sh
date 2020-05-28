1f34920ea41eba92b07793cbbb484d7b45685234
Gleb Galkin <gleb108@gmail.com>
2020-05-28 17:29:17 +0300

    rhosp16 fix for undercloud
    partial-jira-bug: CEM-11349
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I95c47a45a0150e612ddf6a5a8fd130b67040de93

--- rhosp/undercloud/rhel8_deploy_as_root.sh
+++ rhosp/undercloud/rhel8_deploy_as_root.sh
@@ -1,4 +1,7 @@
 
 #RHEL8 undercloud install
 
+#removing docker-ce package to avoid conflicts with podman
+dnf remove -y docker-ce-cli
+
 dnf install -y python3-tripleoclient rhosp-director-images rhosp-director-images-ipa
