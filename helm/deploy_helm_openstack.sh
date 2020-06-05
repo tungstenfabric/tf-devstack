47275ac87da0879c6108ea85764be0992f2eddf2
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-06-05 14:29:11 +0300

    Refactoring Helm Openstack
    Change-Id: I28ed5261a1e7ef5caf30bab9e95a96c2a80926bd

--- helm/deploy_helm_openstack.sh
+++ helm/deploy_helm_openstack.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -xe
 
-set -o errexit
-
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 source "$my_dir/../common/common.sh"
 source "$my_dir/../common/functions.sh"
 
-# parameters
-
-HELM_OPENSTACK_URL=${HELM_OPENSTACK_URL:-https://review.opendev.org/changes/663390/revisions/602747ac7ca361928f7dd1498da70ebdd4fbb157/archive?format=tgz}
-#HELM_OPENSTACK_INFRA_URL=https://github.com/openstack/openstack-helm-infra/archive/24c1cd4514384fe22f3a882d41cf927588b03f2b.tar.gz
-export OPENSTACK_RELEASE=${OPENSTACK_VERSION:-queens}
+#TODO set vars from env properly
+export OPENSTACK_RELEASE=${OPENSTACK_RELEASE:-queens}
 export OSH_OPENSTACK_RELEASE=${OPENSTACK_RELEASE}
+# Disable checks for openstack compute-kit after setup
+export TF_DEPLOYMENT=yes
+# Disable compute-kit tests
+export RUN_HELM_TESTS=no
 
 [ "$(whoami)" == "root" ] && echo "Please run script as non-root user" && exit 1
 
@@ -21,37 +20,18 @@ if [ "$DISTRO" == "centos" ]; then
     sudo yum remove -y pyparsing
     [[ $(sudo service firewalld stop) ]] || true
     sudo yum install -y epel-release
-    sudo yum install -y wget jq nmap bc python-pip python-devel git gcc nfs-utils
-elif [ "$DISTRO" == "ubuntu" ]; then
+    sudo yum install -y wget jq nmap bc python-pip python-devel git gcc nfs-utils libffi-devel openssl-devel
+ elif [ "$DISTRO" == "ubuntu" ]; then
   export DEBIAN_FRONTEND=noninteractive
-  sudo -E apt-get install --no-install-recommends -y \
-        wget ca-certificates git make jq nmap curl uuid-runtime bc python-pip python-dev nfs-common
+  sudo apt install -y jq python3-pip libffi-dev libssl-dev nfs-common
 fi
-sudo -H -E pip install setuptools
-sudo -H -E pip3 install setuptools
-sudo -H -E pip3 install yq
 
-# label nodes
 label_nodes_by_ip openstack-control-plane=enabled $CONTROLLER_NODES
 label_nodes_by_ip openstack-compute-node=enabled $AGENT_NODES
 
-# fetch helm-openstack
-wget $HELM_OPENSTACK_URL -O $WORKSPACE/helm-openstack.tgz
-#wget $HELM_OPENSTACK_INFRA_URL -O helm-openstack-infra.tgz
-mkdir -p $WORKSPACE/openstack-helm
-tar -xzf $WORKSPACE/helm-openstack.tgz -C $WORKSPACE/openstack-helm
-#tar xzf helm-openstack-infra.tgz --strip-components=1 -C openstack-helm-infra
+# get openstack-helm and openstach-helm-infra
 [ ! -d "$WORKSPACE/openstack-helm-infra" ] && git clone http://github.com/openstack/openstack-helm-infra $WORKSPACE/openstack-helm-infra
-
-# Hack neutron deploy to skip agent listing
-sed -i 's/^openstack network agent list/#openstack network_agent list/' $WORKSPACE/openstack-helm/tools/deployment/developer/nfs/160-compute-kit.sh
-
-# add TF overrides
-cp $my_dir/files/libvirt-tf.yaml $WORKSPACE/openstack-helm-infra/libvirt/values_overrides/tf.yaml
-cp $my_dir/files/nova-tf.yaml $WORKSPACE/openstack-helm/nova/values_overrides/tf.yaml
-cp $my_dir/files/neutron-tf.yaml $WORKSPACE/openstack-helm/neutron/values_overrides/tf.yaml
-cp $my_dir/files/keystone-tf.yaml $WORKSPACE/openstack-helm/keystone/values_overrides/tf.yaml
-sed -i "s/openstack_version:.*$/openstack_version: $OSH_OPENSTACK_RELEASE/" $WORKSPACE/openstack-helm/neutron/values_overrides/tf.yaml
+[ ! -d "$WORKSPACE/openstack-helm" ] && git clone http://github.com/openstack/openstack-helm $WORKSPACE/openstack-helm
 
 # build infra charts
 helm init -c
@@ -72,35 +52,51 @@ kill_helm_serve
 helm serve &
 sleep 5
 helm repo add local http://localhost:8879/charts
-make helm-toolkit
-
-export FEATURE_GATES=tf
 
 # TODO: set coredns replicas=1 if one node
-cd $WORKSPACE/openstack-helm-infra
 make helm-toolkit
 make nfs-provisioner
 
-# label nodes
-for node in $(kubectl get nodes --no-headers | cut -d' ' -f1 | head -1); do
-  kubectl label node $node --overwrite openstack-control-plane=enabled
-done
-for node in $(kubectl get nodes --no-headers | cut -d' ' -f1); do
-  kubectl label node $node --overwrite openstack-compute-node=enabled
-done
+# Install openstack CLI not using standard openstack-helm script due to errors in pip3.
+# The errors are lead to Segmantation fault if try to use standard scripts with kubespray k8s
+sudo -H -E pip3 install \
+  -c${UPPER_CONSTRAINTS_FILE:=https://releases.openstack.org/constraints/upper/master} \
+  cmd2 python-openstackclient python-heatclient --ignore-installed --no-binary :all:
+
+sudo -H mkdir -p /etc/openstack
+sudo -H chown -R $(id -un): /etc/openstack
+tee /etc/openstack/clouds.yaml << EOF
+clouds:
+  openstack_helm:
+    region_name: RegionOne
+    identity_api_version: 3
+    auth:
+      username: 'admin'
+      password: 'password'
+      project_name: 'admin'
+      project_domain_name: 'default'
+      user_domain_name: 'default'
+      auth_url: 'http://keystone.openstack.svc.cluster.local/v3'
+EOF
 
 cd $WORKSPACE/openstack-helm
-./tools/deployment/developer/common/020-setup-client.sh
-./tools/deployment/developer/common/030-ingress.sh
-./tools/deployment/developer/nfs/040-nfs-provisioner.sh
-./tools/deployment/developer/nfs/050-mariadb.sh
-./tools/deployment/developer/nfs/060-rabbitmq.sh
-./tools/deployment/developer/nfs/070-memcached.sh
-./tools/deployment/developer/nfs/080-keystone.sh
-./tools/deployment/developer/nfs/090-heat.sh
-./tools/deployment/developer/nfs/120-glance.sh
-./tools/deployment/developer/nfs/150-libvirt.sh
-./tools/deployment/developer/nfs/160-compute-kit.sh
 
-trap - ERR
+# run deploy scripts
+export FEATURE_GATES=tf
+
+#NOTE: Build helm-toolkit, most charts depend on helm-toolkit
+make helm-toolkit
+
+./tools/deployment/component/common/ingress.sh
+./tools/deployment/component/common/mariadb.sh
+./tools/deployment/component/common/memcached.sh
+./tools/deployment/component/common/rabbitmq.sh
+./tools/deployment/component/nfs-provisioner/nfs-provisioner.sh
+./tools/deployment/component/keystone/keystone.sh
+./tools/deployment/component/heat/heat.sh
+./tools/deployment/component/glance/glance.sh
+./tools/deployment/component/compute-kit/tungsten-fabric.sh prepare
+./tools/deployment/component/compute-kit/libvirt.sh
+./tools/deployment/component/compute-kit/compute-kit.sh
+
 kill_helm_serve
