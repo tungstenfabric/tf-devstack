97b24aa376635c2f344e998459952a75df46de44
alexey-mr <alexey.morlang@gmail.com>
2020-06-06 17:28:26 +0300

    RHOSP: added stuff for logs for rhosp
    - check status of stack after deploy to avoid skip errors
    
    - configure undercloud ip route and vip fqdn
    for sanity
    
    Change-Id: I9a0ce20c9ceba9b581eea0d8d35b37181b77f932

--- rhosp/overcloud/06_deploy_overcloud.sh
+++ rhosp/overcloud/06_deploy_overcloud.sh
@@ -11,3 +11,18 @@ source ./rhosp-environment.sh
 #Specific part of deployment
 source $my_dir/${RHOSP_VERSION}_deploy_overcloud.sh
 
+status=$(openstack stack show -f json overcloud | jq ".stack_status")
+if [[ ! "$status" =~ 'COMPLETE' ]] ; then
+  echo "ERROR: failed to deploy overcloud"
+  exit -1
+fi
+
+# patch hosts to resole overcloud by fqdn
+sudo sed -e "/overcloud.${domain}/d" /etc/hosts
+sudo bash -c "echo \"${fixed_vip} overcloud.${domain}\" >> /etc/hosts"
+
+# add ip route for fixed_vip
+sudo ip route add ${fixed_vip}/32 dev br-ctlplane || {
+  echo "WARNING: ip route add ${fixed_vip}/32 dev br-ctlplane is already set"
+  ip route
+}
