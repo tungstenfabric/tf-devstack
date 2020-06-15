07ec432720af6c78bb6c2e001b5a271b8a39246e
Evgeny Shapovalov <eshapovalov.dev@gmail.com>
2020-06-15 15:38:11 +0300

    Plugins should be taken from TF registry
    Description:
    Change nova and neutron plugins after tf deployment
    
    Change-Id: Ife1c354685ced15e464399acc72206f19a92a2b0

--- ansible/run.sh
+++ ansible/run.sh
@@ -93,6 +93,16 @@ function old_ansible_fetch_deployer() {
     sudo rm -rf $WORKSPACE/root
 }
 
+function generate_inventory() {
+
+    export NODE_IP
+    export CONTAINER_REGISTRY
+    export CONTRAIL_CONTAINER_TAG
+    export OPENSTACK_VERSION
+    export USER=$(whoami)
+    $my_dir/../common/jinja2_render.py < $my_dir/files/instances_$ORCHESTRATOR.yaml > $tf_deployer_dir/instances.yaml
+}
+
 function machines() {
     # install required packages
 
@@ -135,12 +145,7 @@ function machines() {
 
     # generate inventory file
 
-    export NODE_IP
-    export CONTAINER_REGISTRY
-    export CONTRAIL_CONTAINER_TAG
-    export OPENSTACK_VERSION
-    export USER=$(whoami)
-    $my_dir/../common/jinja2_render.py < $my_dir/files/instances_$ORCHESTRATOR.yaml > $tf_deployer_dir/instances.yaml
+    generate_inventory
 
     # create Ansible temporary dir under current user to avoid create it under root
     ansible -m "copy" --args="content=c dest='/tmp/rekjreekrbjrekj.txt'" localhost
@@ -186,6 +191,18 @@ function openstack() {
 }
 
 function tf() {
+     if  [[ "$ORCHESTRATOR" == "openstack" ]]; then
+
+        # for openstack it is needed to change plugins before contrail installation
+        # generate inventory file
+        echo "INFO: Change neutron and nova plugins on TF ones"
+        generate_inventory
+
+        sudo -E ansible-playbook -v -e orchestrator=$ORCHESTRATOR \
+                -e config_file=$tf_deployer_dir/instances.yaml \
+                $tf_deployer_dir/playbooks/install_openstack.yml
+
+    fi
     sudo -E ansible-playbook -v -e orchestrator=$ORCHESTRATOR \
         -e config_file=$tf_deployer_dir/instances.yaml \
         $tf_deployer_dir/playbooks/install_contrail.yml
