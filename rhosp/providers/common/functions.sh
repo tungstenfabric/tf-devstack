5de2b967cab6e24787cdfc7ae90d2f29d5dc7dc2
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-17 16:57:10 +0300

    Add RHOSP deployment on bare metal
    Change-Id: I4e4fbcd64de6b7346d8b817408af6a8abb7311e3

--- rhosp/providers/common/functions.sh
+++ rhosp/providers/common/functions.sh
@@ -27,3 +27,92 @@ function patch_docker_configs(){
         return 1
     fi
 }
+
+function collect_stack_details() {
+    local log_dir=$1
+    [ -n "$log_dir" ] || {
+        echo "WARNING: empty log_dir provided.. logs collection skipped"
+        return
+    }
+    source ~/stackrc
+    # collect stack details
+    echo "INFO: collect stack outputs"
+    openstack stack output show -f json --all overcloud | sed 's/\\n/\n/g' > ${log_dir}/stack_outputs.log
+    echo "INFO: collect stack environment"
+    openstack stack environment show -f json overcloud | sed 's/\\n/\n/g' > ${log_dir}/stack_environment.log
+
+    # ensure stack is not failed
+    status=$(openstack stack show -f json overcloud | jq ".stack_status")
+    if [[ ! "$status" =~ 'COMPLETE' ]] ; then
+        echo "ERROR: stack status $status"
+        echo "ERROR: openstack stack failures list"
+        openstack stack failures list --long overcloud | sed 's/\\n/\n/g' | tee ${log_dir}/stack_failures.log
+
+        echo "INFO: collect failed resources"
+        rm -f ${log_dir}/stack_failed_resources.log
+        local name
+        openstack stack resource list --filter status=FAILED -n 10 -f json overcloud | jq -r -c ".[].resource_name" | while read name ; do
+            echo "ERROR: $name" >> ./stack_failed_resources.log
+            openstack stack resource show -f value overcloud $name | sed 's/\\n/\n/g' >> ${log_dir}/stack_failed_resources.log
+        done
+
+        echo "INFO: collect failed deployments"
+        rm -f ${log_dir}/stack_failed_deployments.log
+        local id
+        openstack software deployment list --format json | jq ".[] | select(.status != \"COMPLETE\") | .id" | while read id ; do
+            openstack software deployment show --format value $id | sed 's/\\n/\n/g' >> ${log_dir}/stack_failed_deployments.log
+        done
+    fi
+}
+
+function get_servers_ips() {
+    openstack server list -c Networks -f value | awk -F '=' '{print $NF}' | xargs
+}
+
+function get_servers_ips_by_flavor() {
+    local flavor=$1
+    openstack server list --flavor $flavor -c Networks -f value | awk -F '=' '{print $NF}' | xargs
+}
+
+function collect_overcloud_env() {
+    local heat_admin=$1
+    local ssh_opts=${ssh_opts:-"-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"}
+    if [[ "$DEPLOY_COMPACT_AIO" == "true" ]] ; then
+        CONTROLLER_NODES=$(get_servers_ips_by_flavor control)
+        AGENT_NODES="$CONTROLLER_NODES"
+    else
+        CONTROLLER_NODES=$(get_servers_ips_by_flavor contrail-controller)
+        AGENT_NODES=$(get_servers_ips_by_flavor compute)
+    fi
+    if [[ -f ~/overcloudrc ]]; then
+        source ~/overcloudrc
+        local CONTROLLER_NODE=$(echo ${CONTROLLER_NODES} | awk '{print $1}')
+        internal_vip=$(ssh $ssh_opts $heat_admin@$CONTROLLER_NODE sudo hiera -c /etc/puppet/hiera.yaml internal_api_virtual_ip)
+        os_auth_internal_api_url=$(echo $OS_AUTH_URL | sed "s#[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+#$internal_vip#")
+    fi
+}
+
+function collect_deployment_log() {
+    local heat_admin=$1
+    set +e
+    #Collecting undercloud logs
+    create_log_dir
+    host_name=$(hostname -s)
+    mkdir ${TF_LOG_DIR}/${host_name}
+    collect_system_stats $host_name
+    collect_stack_details ${TF_LOG_DIR}/${host_name}
+
+    #Collecting overcloud logs
+    for ip in $(get_servers_ips); do
+        scp $ssh_opts $my_dir/../common/collect_logs.sh $heat_admin@$ip:
+        ssh $ssh_opts $heat_admin@$ip TF_LOG_DIR="/home/$heat_admin/logs" ./collect_logs.sh create_log_dir
+        ssh $ssh_opts $heat_admin@$ip TF_LOG_DIR="/home/$heat_admin/logs" ./collect_logs.sh collect_docker_logs
+        ssh $ssh_opts $heat_admin@$ip TF_LOG_DIR="/home/$heat_admin/logs" ./collect_logs.sh collect_system_stats
+        ssh $ssh_opts $heat_admin@$ip TF_LOG_DIR="/home/$heat_admin/logs" ./collect_logs.sh collect_contrail_logs
+        host_name=$(ssh $ssh_opts $heat_admin@$ip hostname -s)
+        mkdir ${TF_LOG_DIR}/${host_name}
+        scp -r $ssh_opts $heat_admin@$ip:logs/* ${TF_LOG_DIR}/${host_name}/
+    done
+    tar -czf ${WORKSPACE}/logs.tgz -C ${TF_LOG_DIR}/.. logs
+    set -e
+}
