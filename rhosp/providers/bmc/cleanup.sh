dd2ffb6ae7702d38898939517e0b407a37f25e59
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-17 16:57:10 +0300

    Add RHOSP deployment on bare metal
    Change-Id: I4e4fbcd64de6b7346d8b817408af6a8abb7311e3

---
+++ rhosp/providers/bmc/cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source $HOME/rhosp-environment.sh
+
+ssh stack@${mgmt_ip} << 'EOF'
+sudo subscription-manager unregister
+source stackrc
+servers=$(openstack server list -c Networks -f value | awk -F '=' '{print $NF}')
+for server in $servers; do
+  ssh -T heat-admin@${server} "sudo subscription-manager unregister"
+done
+EOF
+
+rm -rf "$HOME/rhosp-environment.sh"  "$HOME/.tf"
