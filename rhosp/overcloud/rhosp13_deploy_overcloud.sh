56a89ef8422bd6e256f3666b1bf3e8ef94f3f5f5
alexey-mr <alexey.morlang@gmail.com>
2020-06-26 19:48:34 +0300

    Support TLS with FreeIPA for rhosp13 and rhosp16
    Change-Id: I39036d58187b063ded1d81d61e79b4cb8012ca15

--- rhosp/overcloud/rhosp13_deploy_overcloud.sh
+++ rhosp/overcloud/rhosp13_deploy_overcloud.sh
@@ -1,43 +1,51 @@
 
-if [[ "$USE_PREDEPLOYED_NODES" == false ]]; then
-   openstack overcloud deploy --templates tripleo-heat-templates/ \
-            --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
-            -e environment-rhel-registration.yaml \
-            -e tripleo-heat-templates/extraconfig/pre_deploy/rhel-registration/rhel-registration-resource-registry.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
-            -e misc_opts.yaml \
-            -e contrail-parameters.yaml \
-            -e docker_registry.yaml
-else
-   if [[ -z "${overcloud_compute_prov_ip}" ]]; then
-      role_file="tripleo-heat-templates/roles/ContrailAio.yaml"
-      export OVERCLOUD_ROLES="ContrailAio"
-      export ContrailAio_hosts="${overcloud_cont_prov_ip}"
-   else
-      role_file="tripleo-heat-templates/roles_data_contrail_aio.yaml"
-      export OVERCLOUD_ROLES="Controller Compute ContrailController"
-      export Controller_hosts="${overcloud_cont_prov_ip}"
-      export Compute_hosts="${overcloud_compute_prov_ip}"
-      export ContrailController_hosts="${overcloud_ctrlcont_prov_ip}"
-   fi
-   nohup tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh &
-   job=$!
-   openstack overcloud deploy --templates tripleo-heat-templates/ \
-            --roles-file $role_file \
-            --disable-validations \
-            -e environment-rhel-registration.yaml \
-            -e tripleo-heat-templates/extraconfig/pre_deploy/rhel-registration/rhel-registration-resource-registry.yaml \
-            -e tripleo-heat-templates/environments/deployed-server-environment.yaml \
-            -e tripleo-heat-templates/environments/deployed-server-bootstrap-environment-rhel.yaml \
-            -e tripleo-heat-templates/environments/deployed-server-pacemaker-environment.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
-            -e misc_opts.yaml \
-            -e contrail-parameters.yaml \
-            -e ctlplane-assignments.yaml \
-            -e docker_registry.yaml
-   kill $job || true
+tls_env_files=''
+if [[ -n "$ENABLE_TLS" ]] ; then
+  tls_env_files+=' -e tripleo-heat-templates/environments/contrail/contrail-tls.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/ssl/tls-everywhere-endpoints-dns.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/services/haproxy-public-tls-certmonger.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/ssl/enable-internal-tls.yaml'
 fi
+
+rhel_reg_env_files=''
+if [[ "$ENABLE_RHEL_REGISTRATION" == 'true' ]] ; then
+  rhel_reg_env_files+=" -e environment-rhel-registration.yaml"
+  rhel_reg_env_files+=" -e tripleo-heat-templates/extraconfig/pre_deploy/rhel-registration/rhel-registration-resource-registry.yaml"
+fi
+
+role_file="$(pwd)/tripleo-heat-templates/roles_data_contrail_aio.yaml"
+pre_deploy_nodes_env_files=''
+if [[ "$USE_PREDEPLOYED_NODES" == true ]]; then
+  pre_deploy_nodes_env_files+=" --disable-validations"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-environment.yaml"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-bootstrap-environment-rhel.yaml"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-pacemaker-environment.yaml"
+  pre_deploy_nodes_env_files+=" -e ctlplane-assignments.yaml"
+
+  if [[ -z "${overcloud_compute_prov_ip}" ]]; then
+    role_file="$(pwd)/tripleo-heat-templates/roles/ContrailAio.yaml"
+    export OVERCLOUD_ROLES="ContrailAio"
+    export ContrailAio_hosts="${overcloud_cont_prov_ip}"
+  else
+    export OVERCLOUD_ROLES="Controller Compute ContrailController"
+    export Controller_hosts="${overcloud_cont_prov_ip}"
+    export Compute_hosts="${overcloud_compute_prov_ip}"
+    export ContrailController_hosts="${overcloud_ctrlcont_prov_ip}"
+  fi
+  nohup tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh &
+  job=$!
+fi
+
+openstack overcloud deploy --templates tripleo-heat-templates/ \
+  --roles-file $role_file \
+  -e docker_registry.yaml \
+  $rhel_reg_env_files \
+  $pre_deploy_nodes_env_files \
+  -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
+  -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
+  -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
+  $tls_env_files \
+  -e misc_opts.yaml \
+  -e contrail-parameters.yaml
+
+[ -n "$job" ] && kill $job || true
