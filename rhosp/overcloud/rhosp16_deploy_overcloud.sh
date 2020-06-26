f08f02283eb7181f1f9863bcf186e3f0505fd0e7
alexey-mr <alexey.morlang@gmail.com>
2020-06-26 19:48:34 +0300

    Support TLS with FreeIPA for rhosp13 and rhosp16
    Change-Id: I39036d58187b063ded1d81d61e79b4cb8012ca15

--- rhosp/overcloud/rhosp16_deploy_overcloud.sh
+++ rhosp/overcloud/rhosp16_deploy_overcloud.sh
@@ -1,42 +1,53 @@
-
-if [[ "$USE_PREDEPLOYED_NODES" == false ]]; then
-   openstack overcloud deploy --templates tripleo-heat-templates/ \
-             --stack overcloud --libvirt-type kvm \
-             --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
-             -e tripleo-heat-templates/environments/rhsm.yaml \
-             -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-             -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
-             -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
-             -e misc_opts.yaml \
-             -e contrail-parameters.yaml \
-             -e containers-prepare-parameter.yaml \
-             -e rhsm.yaml
-else
-   if [[ -z "${overcloud_compute_prov_ip}" ]]; then
-      export OVERCLOUD_ROLES="ContrailAio"
-      export ContrailAio_hosts="${overcloud_cont_prov_ip}"
-   else
-      export OVERCLOUD_ROLES="Controller Compute ContrailController"
-      export Controller_hosts="${overcloud_cont_prov_ip}"
-      export Compute_hosts="${overcloud_compute_prov_ip}"
-      export ContrailController_hosts="${overcloud_ctrlcont_prov_ip}"
-   fi
-   nohup tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh &
-   job=$!
-   openstack overcloud deploy --templates tripleo-heat-templates/ \
-            --stack overcloud --libvirt-type kvm \
-            --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
-            --disable-validations \
-            -e tripleo-heat-templates/environments/rhsm.yaml \
-            -e tripleo-heat-templates/environments/deployed-server-environment.yaml \
-            -e tripleo-heat-templates/environments/deployed-server-bootstrap-environment-rhel.yaml \
-            -e tripleo-heat-templates/environments/deployed-server-pacemaker-environment.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
-            -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
-            -e misc_opts.yaml \
-            -e contrail-parameters.yaml \
-            -e ctlplane-assignments.yaml \
-            -e rhsm.yaml
-   kill $job || true
+tls_env_files=''
+if [[ -n "$ENABLE_TLS" ]] ; then
+  tls_env_files+=' -e tripleo-heat-templates/environments/contrail/contrail-tls.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/ssl/tls-everywhere-endpoints-dns.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/services/haproxy-public-tls-certmonger.yaml'
+  tls_env_files+=' -e tripleo-heat-templates/environments/ssl/enable-internal-tls.yaml'
 fi
+
+role_file="$(pwd)/tripleo-heat-templates/roles_data_contrail_aio.yaml"
+pre_deploy_nodes_env_files=''
+if [[ "$USE_PREDEPLOYED_NODES" == true ]]; then
+  pre_deploy_nodes_env_files+=" --disable-validations"
+  pre_deploy_nodes_env_files+=" --overcloud-ssh-user $SSH_USER"
+  pre_deploy_nodes_env_files+=" --overcloud-ssh-key .ssh/id_rsa"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-environment.yaml"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-bootstrap-environment-rhel.yaml"
+  pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-pacemaker-environment.yaml"
+  pre_deploy_nodes_env_files+=" -e ctlplane-assignments.yaml"
+
+  if [[ -z "${overcloud_compute_prov_ip}" ]]; then
+    role_file="$(pwd)/tripleo-heat-templates/roles/ContrailAio.yaml"
+    export OVERCLOUD_ROLES="ContrailAio"
+    export ContrailAio_hosts="${overcloud_cont_prov_ip}"
+  else
+    export OVERCLOUD_ROLES="Controller Compute ContrailController"
+    export Controller_hosts="${overcloud_cont_prov_ip}"
+    export Compute_hosts="${overcloud_compute_prov_ip}"
+    export ContrailController_hosts="${overcloud_ctrlcont_prov_ip}"
+  fi
+fi
+
+python3 tripleo-heat-templates/tools/process-templates.py --clean \
+  -r $role_file \
+  -p tripleo-heat-templates/
+
+python3 tripleo-heat-templates/tools/process-templates.py \
+  -r $role_file \
+  -p tripleo-heat-templates/
+
+openstack overcloud deploy --templates tripleo-heat-templates/ \
+  --stack overcloud --libvirt-type kvm \
+  --roles-file $role_file \
+  $rhel_reg_env_files \
+  $pre_deploy_nodes_env_files \
+  -e tripleo-heat-templates/environments/rhsm.yaml \
+  -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
+  -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
+  -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
+  $tls_env_files \
+  -e misc_opts.yaml \
+  -e contrail-parameters.yaml \
+  -e containers-prepare-parameter.yaml \
+  -e rhsm.yaml
--- rhosp/providers/common/rhel8_provisioning.sh
+++ rhosp/providers/common/rhel8_provisioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 dnf update -y
-dnf install -y chrony wget yum-utils vim iproute jq curl bind-utils net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
+dnf install -y chrony wget yum-utils vim iproute jq curl bind-utils network-scripts net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
 
 systemctl start chronyd
