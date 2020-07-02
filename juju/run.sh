eebaeb9c4635ed0298c2163f69ecd67462163a5c
Andrey Pavlov <andrey-mp@yandex.ru>
2020-07-02 14:55:12 +0300

    increase timeouts for juju maas
    Change-Id: I138c6bd32fecec10ec255f5598e06cacfaf68652

--- juju/run.sh
+++ juju/run.sh
@@ -24,8 +24,8 @@ export PATH=$PATH:/snap/bin
 
 # default env variables
 export DEPLOYER='juju'
-# max wait in seconds after deployment (openstack ~ 1300, k8s ~ 2100, maas ~ 2400)
-export WAIT_TIMEOUT=${WAIT_TIMEOUT:-3000}
+# max wait in seconds after deployment (openstack ~ 1300, k8s ~ 2100, maas ~ 2400???3600)
+export WAIT_TIMEOUT=${WAIT_TIMEOUT:-3600}
 export JUJU_REPO=${JUJU_REPO:-$WORKSPACE/tf-charms}
 export ORCHESTRATOR=${ORCHESTRATOR:-kubernetes}  # openstack | kubernetes
 export CLOUD=${CLOUD:-local}  # aws | local | manual
