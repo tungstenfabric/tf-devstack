fa84d17df3e6161fdfb50b1221bcee17da2d346b
Evgeny Shapovalov <eshapovalov.dev@gmail.com>
2020-07-03 16:49:39 +0300

    Rhosp16 CI repo support
    Description:
    Add rhosp version resolution
    Add support of rhel-mirror for containers
    
    Change-Id: I926f3b95c2311e4f808aa39e6e900d7543cce65d

--- rhosp/providers/common/functions.sh
+++ rhosp/providers/common/functions.sh
@@ -10,3 +10,20 @@ function is_registry_insecure() {
     echo "DEBUG: is_registry_insecure: $registry is secure"
     return 1
 }
+
+function set_rhosp_version() {
+    case "$OPENSTACK_VERSION" in
+    "queens" )
+        export RHEL_VERSION='rhel7'
+        export RHOSP_VERSION='rhosp13'
+        ;;
+    "train" )
+        export RHEL_VERSION='rhel8'
+        export RHOSP_VERSION='rhosp16'
+        ;;
+    *)
+        echo "Variable OPENSTACK_VERSION is unset or incorrect"
+        exit 1
+        ;;
+esac
+}
