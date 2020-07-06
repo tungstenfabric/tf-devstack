bdb767b29b5f204d94215bf2a620dafafbdaa1a9
Evgeny Shapovalov <eshapovalov.dev@gmail.com>
2020-07-06 23:04:28 +0300

    RHOSP openstack images local repo support
    Description:
    add support to download images from
    rhel8-mirrors and rhel7-mirrors for vexx installations
    Add parameter OPENSTACK_CONTAINER_REGISTRY
    
    Change-Id: I293e8d8aad4cf0d29f5e78611445e10960513efc

--- rhosp/overcloud/rhosp13_prepare_containers.sh
+++ rhosp/overcloud/rhosp13_prepare_containers.sh
@@ -1,17 +1,18 @@
-
+#!/bin/bash -ex
 openstack overcloud container image prepare \
-  --namespace registry.access.redhat.com/rhosp13  --prefix=openstack- --tag-from-label {version}-{release} \
+  --namespace ${OPENSTACK_CONTAINER_REGISTRY}/rhosp13  --prefix=openstack- --tag-from-label {version} \
   --push-destination ${prov_ip}:8787 \
   --output-env-file ./docker_registry.yaml \
-  --output-images-file ./overcloud_containers.yaml
+  --output-images-file ./overcloud_containers.yaml \
+  --tag '13.0'
 
 echo 'openstack overcloud container image upload --config-file ./overcloud_containers.yaml'
 openstack overcloud container image upload --config-file ./overcloud_containers.yaml
 
 registry=${CONTAINER_REGISTRY:-'docker.io/tungstenfabric'}
-tag=${CONTRAIL_CONTAINER_TAG:-'latest'}
+contrail_tag=${CONTRAIL_CONTAINER_TAG:-'latest'}
 ./contrail-tripleo-heat-templates/tools/contrail/import_contrail_container.sh \
-    -f ./contrail_containers.yaml -r $registry -t $tag
+    -f ./contrail_containers.yaml -r $registry -t $contrail_tag
 
 sed -i ./contrail_containers.yaml -e "s/192.168.24.1/${prov_ip}/"
 cat ./contrail_containers.yaml
