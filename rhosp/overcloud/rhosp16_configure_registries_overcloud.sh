c54411b9c73ba8dae3067aab1af0b8b001c1e285
Evgeny Shapovalov <eshapovalov.dev@gmail.com>
2020-07-06 23:04:28 +0300

    RHOSP openstack images local repo support
    Description:
    add support to download images from
    rhel8-mirrors and rhel7-mirrors for vexx installations
    Add parameter OPENSTACK_CONTAINER_REGISTRY
    
    Change-Id: I293e8d8aad4cf0d29f5e78611445e10960513efc

--- rhosp/overcloud/rhosp16_configure_registries_overcloud.sh
+++ rhosp/overcloud/rhosp16_configure_registries_overcloud.sh
@@ -1,7 +1,10 @@
-if [ -n "$CONTAINER_REGISTRY" ] && is_registry_insecure $CONTAINER_REGISTRY ; then
+if [[ -n "$CONTAINER_REGISTRY" ]] ; then
     registries_file="/etc/containers/registries.conf"
     current_registries="$(sed -n '/registries.insecure/{n; s/registries = //p}' "$registries_file" | tr -d '[]')"
     changed_registries="[$current_registries,'$CONTAINER_REGISTRY']"
+    [ -n "$OPENSTACK_CONTAINER_REGISTRY" ] && changed_registries="[$current_registries,'$CONTAINER_REGISTRY', '$OPENSTACK_CONTAINER_REGISTRY']"
     echo "INFO: new registries are $changed_registries"
-    sed -i "/registries.insecure/{n; s/registries = .*$/${changed_registries}/g}" ${registries_file}
+    sudo sed "/registries.insecure/{n; s/registries = .*$/${changed_registries}/g}" ${registries_file} > ${registries_file}.temp
+    sudo cp -f ${registries_file}.temp ${registries_file}
+    rm -rf ${registries_file}.temp
 fi
