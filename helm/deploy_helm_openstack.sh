c7fd79759f7b3106ba3d26f7ed06986150324c36
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-07-20 14:24:13 +0300

    Added install appdirs before bug
    Change-Id: I42f749a57fecbebe1c788734b7d69669500b92ac

--- helm/deploy_helm_openstack.sh
+++ helm/deploy_helm_openstack.sh
@@ -60,6 +60,10 @@ make nfs-provisioner
 # The errors are lead to Segmantation fault if try to use standard scripts with kubespray k8s
 sudo -H -E pip3 install \
   -c${UPPER_CONSTRAINTS_FILE:=https://releases.openstack.org/constraints/upper/master} \
+  appdirs --ignore-installed --no-binary :all:
+sleep 12
+sudo -H -E pip3 install \
+  -c${UPPER_CONSTRAINTS_FILE:=https://releases.openstack.org/constraints/upper/master} \
   cmd2 python-openstackclient python-heatclient --ignore-installed --no-binary :all:
 
 sudo -H mkdir -p /etc/openstack
