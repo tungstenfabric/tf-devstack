8bc4c3fe82a1038959910b54dcf07c35dab066bd
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-07-21 03:55:28 +0300

    Added exit for debug
    Change-Id: I8a069327916e6e46f3de84873b7cc59475e74f52

--- helm/deploy_helm_openstack.sh
+++ helm/deploy_helm_openstack.sh
@@ -58,6 +58,10 @@ make nfs-provisioner
 
 # Install openstack CLI not using standard openstack-helm script due to errors in pip3.
 # The errors are lead to Segmantation fault if try to use standard scripts with kubespray k8s
+echo "0000000000000000000000000000000000000000"
+echo "\033[33mDo exit for debug"
+echo "0000000000000000000000000000000000000000"
+exit 1
 sudo -H -E pip3 install \
   -c${UPPER_CONSTRAINTS_FILE:=https://releases.openstack.org/constraints/upper/master} \
   cmd2 python-openstackclient python-heatclient --ignore-installed --no-binary :all:
