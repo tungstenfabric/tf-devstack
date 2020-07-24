28c24f817caeb4daf5b54286f33ed009cb07be1e
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-07-24 18:28:57 +0300

    Fix launch sanity test for rhosp13-bmc
    depends-on: Ie2ccb69a87002df103fc7a07a1238f6f23736468
    partial-jira-bug: CEM-17543
    Change-Id: Ibcdb97b65ddae0721b7a1400b01542c76fb23fa4

--- rhosp/overcloud/06_deploy_overcloud.sh
+++ rhosp/overcloud/06_deploy_overcloud.sh
@@ -17,14 +17,3 @@ if [[ ! "$status" =~ 'COMPLETE' || -z "$status" ]] ; then
   echo "ERROR: failed to deploy overcloud: status=$status"
   exit -1
 fi
-
-CONTROLLER_NODE=$(get_servers_ips_by_flavor control | awk '{print $1}')
-
-if [[ -n "$CONTROLLER_NODE" ]]; then
-  # patch hosts to resole overcloud by fqdn
-  sudo sed -e "/overcloud.${domain}/d" /etc/hosts
-  sudo bash -c "echo \"${overcloud_cont_prov_ip} overcloud.${domain}\" >> /etc/hosts"
-  sudo sed -e "/overcloud.internalapi.${domain}/d" /etc/hosts
-  sudo bash -c "echo \"${overcloud_cont_prov_ip} overcloud.internalapi.${domain}\" >> /etc/hosts"
-  sudo bash -c "echo \"${overcloud_cont_prov_ip} overcloud.ctlplane.${domain}\" >> /etc/hosts"
-fi
