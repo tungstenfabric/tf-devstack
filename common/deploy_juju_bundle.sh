2aecd634a9b66b98f35262cb6e983975cfe6e01e
Andrey Pavlov <andrey-mp@yandex.ru>
2020-08-17 10:59:08 +0300

    apply workaround to handle lxd issue with apt-get update
    Change-Id: Ib1312dc6b29af5e89d3a25bb474af1f440c7baa4

--- common/deploy_juju_bundle.sh
+++ common/deploy_juju_bundle.sh
@@ -4,6 +4,7 @@ set -o errexit
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 source "$my_dir/common.sh"
+source "$my_dir/functions.sh"
 
 # parameters
 BUNDLE=${BUNDLE:-} # may be template, would be rendered by jinja
@@ -21,3 +22,19 @@ cat "$WORKSPACE/bundle.yaml"
 
 echo "INFO: Run bundle..."
 juju deploy --debug $WORKSPACE/bundle.yaml --map-machines=existing
+
+# workaround an issue with inability to install python-pip3 in lxd container
+# due to incorrect handling of apt-get update in cloud-init the operation may silently fail
+# and next calls to apt-get may fail
+# call apt-get update manulayy to prevent this issue
+if ! grep -q "lxd:" $WORKSPACE/bundle.yaml ; then
+  exit
+fi
+# wait a bit while juju creates lxd machines in it's database
+sleep 30
+lxd_machines=`timeout -s 9 30 juju machines --format tabular | tail -n +2 | grep "\/lxd\/" | awk '{print $1}'`
+echo "INFO: lxd machines:"
+echo $lxd_machines
+for machine in $lxd_machines ; do
+  wait_cmd_success 'juju ssh $machine "sudo apt-get update -y"'
+done
