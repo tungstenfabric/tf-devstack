6bfc6868f8809e9670be5b0ab973a75649437b82
Inna M <imitryaeva@progmaticlab.com>
2020-08-19 11:48:58 +0000

    Apps removal added before controller destruction
    Change-Id: I5fe700ae882e2c194dc85a2de7b7f5719956b44e

--- juju/cleanup.sh
+++ juju/cleanup.sh
@@ -5,13 +5,28 @@ set -o errexit
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 source "$my_dir/../common/common.sh"
+source "$my_dir/../common/functions.sh"
 
 export CLOUD=${CLOUD:-local}  # aws | local | manual
 
 rm -rf ~/.tf/.stages
 
 echo "Destroying tf-$CLOUD-controller controller, machines, applications and data."
-juju kill-controller -y tf-$CLOUD-controller || juju unregister -y tf-$CLOUD-controller
+if [ -n "$(juju status --format json | jq '.applications' | jq -r 'keys[]')" ]; then
+	juju status --format json | jq '.applications' | jq -r 'keys[]' | xargs -n 1 juju remove-application --force
+fi
+
+function no_apps_left() {
+	if [ -z "$(juju status --format json | jq '.applications' | jq -r 'keys[]')" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+wait_cmd_success "no_apps_left"
+
+juju destroy-controller -y --destroy-all-models tf-$CLOUD-controller
 
 [[ $CLOUD == 'maas' ]] && echo "Cleanup is over." && exit
 
@@ -20,11 +35,14 @@ sudo rm -rf /var/lib/juju
 sudo rm -rf /lib/systemd/system/juju*
 sudo rm -rf /run/systemd/units/invocation:juju*
 sudo rm -rf /etc/systemd/system/juju*
+sudo rm -rf /etc/contrail/
+sudo rm -rf /etc/docker
 
 if [[ $CLOUD == 'manual' ]] && [[ -n "$CONTROLLER_NODES" || -n "$AGENT_NODES" ]]; then
     echo "Clean other nodes." 
     for machine in $CONTROLLER_NODES $AGENT_NODES ; do
-        ssh ubuntu@$machine "sudo rm -rf /var/lib/juju ; sudo rm -rf /lib/systemd/system/juju* ; sudo rm -rf /run/systemd/units/invocation:juju* ; sudo rm -rf /etc/systemd/system/juju*"
+        ssh ubuntu@$machine "sudo rm -rf /var/lib/juju ; sudo rm -rf /lib/systemd/system/juju* ; sudo rm -rf /run/systemd/units/invocation:juju* ; sudo rm -rf /etc/systemd/system/juju* ;
+	sudo rm -rf /etc/contrail/ ; sudo rm -rf /etc/docker"
     done
 fi
 
