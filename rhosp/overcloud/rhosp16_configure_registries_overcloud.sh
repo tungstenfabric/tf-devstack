d591a80b3bfeeb6f480db80435d3fa217ef33786
Arina Altynova <aaltynova@progmaticlab.com>
2020-08-21 15:05:59 +0300

    [~]  configure registries improvement
    Change-Id: Idc30164c59c3cab576986cb6e735c058917db1ed

--- rhosp/overcloud/rhosp16_configure_registries_overcloud.sh
+++ rhosp/overcloud/rhosp16_configure_registries_overcloud.sh
@@ -3,11 +3,9 @@ registries_file="/etc/containers/registries.conf"
 current_registries="$(sed -n '/registries.insecure/{n; s/registries = //p}' "$registries_file" | tr -d '[]')"
 echo "INFO: old registries are $current_registries"
 changed_registries=""
-[ -n "$current_registries" ] && changed_registries+="$current_registries "
+[ -n "$current_registries" ] && changed_registries+="$current_registries, "
 if ! echo "$current_registries" | grep -q "${prov_ip}:8787" ; then
-   changed_registries+="'${prov_ip}:8787' "
-   changed_registries=$(echo "$changed_registries" | sed 's/ \+/ /g')
-   changed_registries=$(echo "$changed_registries" | sed 's/ /,/g')
+   changed_registries+="'${prov_ip}:8787'"
    changed_registries="registries = [$changed_registries]"
    echo "INFO: new registries are $changed_registries"
    sudo sed "/registries.insecure/{n; s/registries = .*$/${changed_registries}/g}" ${registries_file} > registries.conf.tmp
--- rhosp/undercloud/rhosp16_configure_registries_undercloud.sh
+++ rhosp/undercloud/rhosp16_configure_registries_undercloud.sh
@@ -10,15 +10,13 @@ registries_file="/etc/containers/registries.conf"
 current_registries="$(sed -n '/registries.insecure/{n; s/registries = //p}' "$registries_file" | tr -d '[]')"
 echo "INFO: old registries are $current_registries"
 changed_registries=""
-[ -n "$current_registries" ] && changed_registries+="$current_registries "
+[ -n "$current_registries" ] && changed_registries+="$current_registries, "
 if [[ -n "$CONTAINER_REGISTRY"  ]] && is_registry_insecure "$CONTAINER_REGISTRY" ; then
-    changed_registries+="'$CONTAINER_REGISTRY' "
+    changed_registries+="'$CONTAINER_REGISTRY'"
 fi
 if [[ -n "$OPENSTACK_CONTAINER_REGISTRY" ]]  && is_registry_insecure "$OPENSTACK_CONTAINER_REGISTRY" ; then
-    changed_registries+="'$OPENSTACK_CONTAINER_REGISTRY' "
+    changed_registries+="'$OPENSTACK_CONTAINER_REGISTRY'"
 fi
-changed_registries=$(echo "$changed_registries" | sed 's/ \+/ /g')
-changed_registries=$(echo "$changed_registries" | sed 's/ /,/g')
 changed_registries="registries = [$changed_registries]"
 echo "INFO: new registries are $changed_registries"
 sudo sed "/registries.insecure/{n; s/registries = .*$/${changed_registries}/g}" ${registries_file} > registries.conf.tmp
