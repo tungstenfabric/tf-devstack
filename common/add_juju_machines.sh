2734969a5a26dd0c6e11d50a4c0cdf8b841e9303
tikitavi <rtikitavi@gmail.com>
2020-08-25 16:54:14 +0300

    DNM juju: add orchestrator all on manual cloud
    WIP: unify the juju bundles
    
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Change-Id: I4ecd18234520935eb06b13e2ad83eae9ffedd295

--- common/add_juju_machines.sh
+++ common/add_juju_machines.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Adds CONTROLLER_NODES to model for manual deployment
-
 set -o errexit
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 source "$my_dir/common.sh"
 
+# Adds CONTROLLER_NODES and AGENT_NODES to model for manual deployment
 if [[ -n "$CONTROLLER_NODES" || -n "$AGENT_NODES" ]]; then
     for node in $CONTROLLER_NODES $AGENT_NODES ; do
-        if [[ $node != $NODE_IP && $CLOUD == 'manual' ]]; then
-            juju add-machine ssh:ubuntu@$node
+        if [[ $node != $NODE_IP && $CLOUD == "manual" ]]; then
+                juju add-machine ssh:ubuntu@$node
         fi
     done
 fi
