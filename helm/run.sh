938cb055f91b344723545ea5d74bbdf9fc34eabb
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-09-10 17:11:06 +0300

    Add multi-nodes logs for helm
    Change-Id: I8aa8fa7785fb2480f7cd4dc7626c2fe0b20d5988

--- helm/run.sh
+++ helm/run.sh
@@ -54,22 +54,52 @@ function build() {
 function logs() {
     local errexit_state=$(echo $SHELLOPTS| grep errexit | wc -l)
     set +e
-
-    if [ "$DISTRO" == "centos" ]; then
-        sudo yum install -y lsof jq
-    elif [ "$DISTRO" == "ubuntu" ]; then
-        export DEBIAN_FRONTEND=noninteractive
-        sudo -E apt-get install -y lsof jq
-    fi
-
     create_log_dir
     cp $WORKSPACE/tf-devstack-values.yaml ${TF_LOG_DIR}/
-    collect_docker_logs
-    collect_contrail_status
-    collect_system_stats
-    collect_kubernetes_logs
-    collect_kubernetes_objects_info
-    collect_contrail_logs
+
+    cat <<EOF >/tmp/logs.sh
+#!/bin/bash
+tgz_name=\$1
+export WORKSPACE=/tmp/helm-logs
+export TF_LOG_DIR=/tmp/helm-logs/logs
+export SSL_ENABLE=$SSL_ENABLE
+DISTRO=$(cat /etc/*release | egrep '^ID=' | awk -F= '{print $2}' | tr -d \")
+if [ "$DISTRO" == "centos" ]; then
+    sudo yum install -y lsof jq
+elif [ "$DISTRO" == "ubuntu" ]; then
+    export DEBIAN_FRONTEND=noninteractive
+    sudo -E apt-get install -y lsof jq
+fi
+cd /tmp/helm-logs
+source ./collect_logs.sh
+collect_docker_logs
+collect_contrail_status
+collect_system_stats
+collect_kubernetes_logs
+collect_kubernetes_objects_info
+collect_contrail_logs
+chmod -R a+r logs
+pushd logs
+tar -czf \$tgz_name *
+popd
+cp logs/\$tgz_name \$tgz_name
+rm -rf logs
+EOF
+chmod a+x /tmp/logs.sh
+
+    for machine in $(echo "$CONTROLLER_NODES $AGENT_NODES" | tr " " "\n" | sort -u) ; do
+        local tgz_name="logs-$machine.tgz"
+        mkdir -p $TF_LOG_DIR/$machine
+        ssh $machine "mkdir -p /tmp/helm-logs"
+        scp $my_dir/../common/collect_logs.sh $machine:/tmp/helm-logs/collect_logs.sh
+        scp /tmp/logs.sh $machine:/tmp/helm-logs/logs.sh
+        ssh $machine /tmp/helm-logs/logs.sh $tgz_name
+        scp $machine:/tmp/helm-logs/$tgz_name $TF_LOG_DIR/$machine/
+        pushd $TF_LOG_DIR/$machine/
+        tar -xzf $tgz_name
+        rm -rf $tgz_name
+        popd
+    done
 
     tar -czf ${WORKSPACE}/logs.tgz -C ${TF_LOG_DIR}/.. logs
     rm -rf $TF_LOG_DIR
