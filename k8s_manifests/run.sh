242471b4ebef29cdc6ead623d69fa6070ddef1c5
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-09-15 17:31:42 +0300

    Add ssh options for helm, k8s manifests, ansible
    configure ssh to not check host keys and avoid garbadge in known hosts files
    
    Change-Id: I12d9f4b2bd1dfc66ed07738ee91f103603a4b8ef

--- k8s_manifests/run.sh
+++ k8s_manifests/run.sh
@@ -70,14 +70,16 @@ rm -rf logs
 EOF
 chmod a+x /tmp/logs.sh
 
+ssh_opts="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
     for machine in $(echo "$CONTROLLER_NODES $AGENT_NODES" | tr " " "\n" | sort -u) ; do
         local tgz_name="logs-$machine.tgz"
         mkdir -p $TF_LOG_DIR/$machine
-        ssh $machine "mkdir -p /tmp/k8s_manifests-logs"
-        scp $my_dir/../common/collect_logs.sh $machine:/tmp/k8s_manifests-logs/collect_logs.sh
-        scp /tmp/logs.sh $machine:/tmp/k8s_manifests-logs/logs.sh
-        ssh $machine /tmp/k8s_manifests-logs/logs.sh $tgz_name
-        scp $machine:/tmp/k8s_manifests-logs/$tgz_name $TF_LOG_DIR/$machine/
+        ssh $ssh_opts $machine "mkdir -p /tmp/k8s_manifests-logs"
+        scp $ssh_opts $my_dir/../common/collect_logs.sh $machine:/tmp/k8s_manifests-logs/collect_logs.sh
+        scp $ssh_opts /tmp/logs.sh $machine:/tmp/k8s_manifests-logs/logs.sh
+        ssh $ssh_opts $machine /tmp/k8s_manifests-logs/logs.sh $tgz_name
+        scp $ssh_opts $machine:/tmp/k8s_manifests-logs/$tgz_name $TF_LOG_DIR/$machine/
         pushd $TF_LOG_DIR/$machine/
         tar -xzf $tgz_name
         rm -rf $tgz_name
