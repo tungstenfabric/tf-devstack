e62c9982fb60ae1db838157c8e269656eb1dffec
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-09-17 14:20:24 +0300

    Enable dpdk and sriov for juju-maas
    Partial-Jira-Bug: CEM-19139
    Change-Id: I29b27109e751a3551b9a8653b13ef57e978ffc40

--- common/functions.sh
+++ common/functions.sh
@@ -190,3 +190,27 @@ function setup_timeserver() {
     return 1
   fi
 }
+
+function get_juju_unit_ips(){
+  local unit=$1
+  local unit_list="`command juju status | grep "$unit" | grep "/" | tr -d "*" | awk '{print $1}'`"
+  local machine_ips
+  local u
+  local ip
+  local ips
+    for u in $unit_list; do
+        unit_machine="`command juju show-unit --format json $u | jq -r .[].machine`"
+        machine_ips="`command juju show-machine --format json $unit_machine | jq -r '.machines[]."ip-addresses"[]'`"
+        if  [[ "`echo $machine_ips | wc -w`" == 1 ]] ; then
+            ips+=" $machine_ips"
+        else
+            for ip in $machine_ips ; do
+                if nc -z $ip 22; then
+                    ips+=" $ip"
+                    break
+                fi
+            done
+        fi
+    done
+  echo $ips
+}
