9e8a986f498450ba4a48111f941ab7dc32c6bdbe
aaalt <aargentova@progmaticlab.com>
2020-09-17 14:43:05 +0300

    [+]  disable selinux
    Change-Id: I97a776220fd9cd231881f51480d606933ac760e7

--- rhosp/providers/common/rhel8_provisioning.sh
+++ rhosp/providers/common/rhel8_provisioning.sh
@@ -3,4 +3,6 @@
 dnf update -y
 dnf install -y --allowerasing chrony wget yum-utils vim iproute jq curl bind-utils network-scripts net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
 
+echo "(allow svirt_tcg_t container_file_t ( dir ( read  )))"  >> /tmp/contrail_container.cil
+sudo /sbin/semodule -i /tmp/contrail_container.cil
 systemctl start chronyd
