2cf46a1d0ce3f8637d7662cb5c03c7b42cd7e175
Kirill Egorov <kegorov@progmaticlab.com>
2020-09-21 15:50:13 +0300

    Fix problem when error marks as success
    Change-Id: I803e1a504076e9b23b498269d5048bf28f8e8c6b

--- rhosp/config/common.sh
+++ rhosp/config/common.sh
@@ -5,7 +5,9 @@ export RHEL_POOL_ID=8a85f99970453685017057d235142b3b
 export undercloud_local_interface=${undercloud_local_interface:-"eth1"}
 export keystone_admin_api_network=${keystone_admin_api_network:-"ctlplane"}
 
-state="$(set +o); set -$(echo $- | tr -d 'i')"
+state="$(set +o)"
+[[ "$-" =~ e ]] && state+="; set -e"
+
 set +x
 #Red Hat credentials
 export RHEL_PASSWORD=$RHEL_PASSWORD
