47040a8d035b4b924c33f9a9285baf1ebcfd7771
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-09-23 07:53:50 +0300

    helm test with debug dry-run
    Change-Id: I0732533b6e6b6c454d01393c3db92740dcb07ec0

--- helm/deploy_tf_helm.sh
+++ helm/deploy_tf_helm.sh
@@ -15,7 +15,8 @@ else
   CONTRAIL_CHART="contrail"
 fi
 
-fetch_deployer $deployer_image $deployer_dir || git clone "$TF_HELM_URL" $deployer_dir
+git clone "$TF_HELM_URL" $deployer_dir
+git review -d 61473
 
 # label nodes
 label_nodes_by_ip opencontrail.org/vrouter-kernel=enabled $AGENT_NODES
@@ -94,9 +95,8 @@ else
   host_var=""
 fi
 
-sudo mkdir -p /var/log/contrail
-
 kubectl create ns tungsten-fabric || :
+helm upgrade --install --debug --dry-run --namespace tungsten-fabric tungsten-fabric $WORKSPACE/tf-helm-deployer/$CONTRAIL_CHART -f $WORKSPACE/tf-devstack-values.yaml $host_var
 helm upgrade --install --namespace tungsten-fabric tungsten-fabric $WORKSPACE/tf-helm-deployer/$CONTRAIL_CHART -f $WORKSPACE/tf-devstack-values.yaml $host_var
 if [ "$ORCHESTRATOR" == "kubernetes" ]; then
   kubectl -n kube-system scale deployment tiller-deploy --replicas=1
