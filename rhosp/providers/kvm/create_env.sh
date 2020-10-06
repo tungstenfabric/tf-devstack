e0a70d8fe0d7619ff047ffa1a42cb5b10635db07
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I6aa7dd807cbe47e1196520c0d3957020757583ae
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

---
+++ rhosp/providers/kvm/create_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/../../../common/common.sh"
+source "$my_dir/../common/common.sh"
+source "$my_dir/../common/functions.sh"
+
+if [[ "${USE_PREDEPLOYED_NODES,,}" == true ]]; then
+  echo "ERROR: unsupported configuration for kvm: USE_PREDEPLOYED_NODES=$USE_PREDEPLOYED_NODES"
+  exit -1
+fi
+
+cd $WORKSPACE
+prepare_rhosp_env_file rhosp-environment.sh
+
+$my_dir/01_create_env.sh
+wait_ssh ${instance_ip} ${ssh_private_key}
+
+$my_dir/02_collecting_node_information.sh
+
+scp $ssh_opts ${ssh_private_key} ${ssh_public_key} root@${instance_ip}:./.ssh/
+scp $ssh_opts ${ssh_private_key} ${ssh_public_key} $SSH_USER@${instance_ip}:./.ssh/
+tf_dir=$(readlink -e $my_dir/../../..)
+rsync -a -e "ssh -i $ssh_private_key $ssh_opts" rhosp-environment.sh $tf_dir $SSH_USER@$instance_ip:
+ssh $ssh_opts -i $ssh_private_key $SSH_USER@${instance_ip} sed -i 's/PROVIDER=.*/PROVIDER=bmc/g' rhosp-environment.sh
+if [[ -n "$ENABLE_TLS" ]] ; then
+  wait_ssh ${ipa_mgmt_ip} ${ssh_private_key}
+  rsync -a -e "ssh -i $ssh_private_key $ssh_opts" rhosp-environment.sh $tf_dir root@${ipa_mgmt_ip}:
+  ssh $ssh_opts -i $ssh_private_key root@${ipa_mgmt_ip} sed -i 's/PROVIDER=.*/PROVIDER=bmc/g' rhosp-environment.sh
+fi
