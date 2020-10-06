ea8379f727348e0c2e7727f4eb187acb08060241
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/overcloud/rhosp13_deploy_overcloud.sh
+++ rhosp/overcloud/rhosp13_deploy_overcloud.sh
@@ -5,6 +5,9 @@ if [[ -n "$ENABLE_TLS" ]] ; then
   tls_env_files+=' -e tripleo-heat-templates/environments/ssl/tls-everywhere-endpoints-dns.yaml'
   tls_env_files+=' -e tripleo-heat-templates/environments/services/haproxy-public-tls-certmonger.yaml'
   tls_env_files+=' -e tripleo-heat-templates/environments/ssl/enable-internal-tls.yaml'
+else
+  # use names even w/o tls case
+  tls_env_files+=' -e tripleo-heat-templates/environments/contrail/endpoints-public-dns.yaml'
 fi
 
 rhel_reg_env_files=''
@@ -22,7 +25,7 @@ else
 fi
 
 storage_env_files=''
-if [[ "$backend_storage" == "rbd" ]] ; then
+if [[ -n "$overcloud_ceph_instance" ]] ; then
     storage_env_files+=' -e tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml'
     storage_env_files+=' -e tripleo-heat-templates/environments/ceph-ansible/ceph-mds.yaml'
 fi
@@ -32,7 +35,17 @@ if [[ "${DEPLOY_COMPACT_AIO,,}" == 'true' ]] ; then
 else
   role_file="$(pwd)/tripleo-heat-templates/roles_data_contrail_aio.yaml"
 fi
+
+./tripleo-heat-templates/tools/process-templates.py --clean \
+  -r $role_file \
+  -p tripleo-heat-templates/
+
+./tripleo-heat-templates/tools/process-templates.py \
+  -r $role_file \
+  -p tripleo-heat-templates/
+
 pre_deploy_nodes_env_files=''
+job=''
 if [[ "$USE_PREDEPLOYED_NODES" == true ]]; then
   pre_deploy_nodes_env_files+=" --disable-validations"
   pre_deploy_nodes_env_files+=" -e tripleo-heat-templates/environments/deployed-server-environment.yaml"
@@ -53,14 +66,6 @@ if [[ "$USE_PREDEPLOYED_NODES" == true ]]; then
   job=$!
 fi
 
-./tripleo-heat-templates/tools/process-templates.py --clean \
-  -r $role_file \
-  -p tripleo-heat-templates/
-
-./tripleo-heat-templates/tools/process-templates.py \
-  -r $role_file \
-  -p tripleo-heat-templates/
-
 openstack overcloud deploy --templates tripleo-heat-templates/ \
   --roles-file $role_file \
   -e docker_registry.yaml \
@@ -69,7 +74,6 @@ openstack overcloud deploy --templates tripleo-heat-templates/ \
   -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
   $network_env_files \
   $storage_env_files \
-  -e tripleo-heat-templates/environments/contrail/endpoints-public-dns.yaml \
   -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
   $tls_env_files \
   -e misc_opts.yaml \
--- rhosp/overcloud/rhosp13_manage_overcloud_flavors.sh
+++
@@ -1,4 +0,0 @@
-  openstack flavor create --id auto --ram 1000 --disk 29 --vcpus 2 baremetal
-  openstack flavor set --property "cpu_arch"="x86_64" \
-                       --property "capabilities:boot_option"="local" \
-                       baremetal
