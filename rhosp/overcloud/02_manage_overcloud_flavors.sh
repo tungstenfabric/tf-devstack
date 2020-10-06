6b60021f6c4886973dba125c3caec2807972aca4
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I47146e6f0fa6834bf99487a61034fda1c9c3e251
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/overcloud/02_manage_overcloud_flavors.sh
+++ rhosp/overcloud/02_manage_overcloud_flavors.sh
@@ -4,12 +4,18 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 cd
-source rhosp-environment.sh
 source stackrc
+source rhosp-environment.sh
+source $my_dir/../../common/common.sh
 
 # re-define flavors
 for id in `openstack flavor list -f value -c ID` ; do openstack flavor delete $id ; done
 
-#Specific part of deployment
-source $my_dir/${RHOSP_VERSION}_manage_overcloud_flavors.sh
-
+openstack flavor create --id auto --ram 1000 --disk 29 --vcpus 2 baremetal
+openstack flavor set --property "cpu_arch"="x86_64" \
+                     --property "capabilities:boot_option"="local" \
+                     --property "resources:CUSTOM_BAREMETAL=1" \
+                     --property "resources:DISK_GB=0" \
+                     --property "resources:MEMORY_MB=0" \
+                     --property "resources:VCPU=0" \
+                      baremetal
