9519ca6680c65938c742f6e1a7d8f063892cee71
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/providers/bmc/cleanup.sh
+++ rhosp/providers/bmc/cleanup.sh
@@ -3,11 +3,16 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-sudo subscription-manager unregister
-source ~/stackrc
-servers=$(openstack server list -c Networks -f value | awk -F '=' '{print $NF}')
-for server in $servers; do
-  ssh -T heat-admin@${server} "sudo subscription-manager unregister"
-done
+cd
+source rhosp-environment.sh
+source stackrc
+
+if [[ "$ENABLE_RHEL_REGISTRATION" == 'true' ]] ; then
+  sudo subscription-manager unregister
+  servers=$(openstack server list -c Networks -f value | awk -F '=' '{print $NF}')
+  for server in $servers; do
+    ssh -T heat-admin@${server} "sudo subscription-manager unregister"
+  done
+fi
 
 rm -rf "$HOME/rhosp-environment.sh" "$HOME/.tf"
