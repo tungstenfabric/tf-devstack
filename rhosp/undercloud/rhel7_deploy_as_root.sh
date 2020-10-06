0beed681ee3848456f3a06a42f8a7017507faaa0
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I6aa7dd807cbe47e1196520c0d3957020757583ae
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/undercloud/rhel7_deploy_as_root.sh
+++
@@ -1,6 +0,0 @@
-
-if [[ "${ENABLE_RHEL_REGISTRATION}" == 'true' ]] ; then
-   yum-config-manager --enable rhelosp-rhel-7-server-opt
-fi
-
-yum -y install python-tripleoclient python-rdomanager-oscplugin iproute rhosp-director-images
