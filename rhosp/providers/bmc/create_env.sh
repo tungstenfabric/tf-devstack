81f302d6f5e7362addae083f3420bc97acdbe286
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I9cc0b98a6284d0b3653f2061461a58371d684a2a
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

---
+++ rhosp/providers/bmc/create_env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/../../../common/common.sh"
+source "$my_dir/../common/common.sh"
+source "$my_dir/../common/functions.sh"
+
+if [[ "${USE_PREDEPLOYED_NODES,,}" == true ]]; then
+  echo "ERROR: unsupported configuration for kvm: USE_PREDEPLOYED_NODES=$USE_PREDEPLOYED_NODES"
+  exit -1
+fi
+
+# SSH public key for user stack
+export ssh_private_key=${ssh_private_key:-~/.ssh/workers}
+
+# env is created externally, just check availability
+wait_ssh ${instance_ip} ${ssh_private_key}
+
+cd $WORKSPACE
+prepare_rhosp_env_file rhosp-environment.sh
+tf_dir=$(readlink -e $my_dir/../../..)
+rsync -a -e "ssh -i $ssh_private_key $ssh_opts" rhosp-environment.sh $tf_dir $SSH_USER@$instance_ip:
+
+if [[ -n "$ENABLE_TLS" ]] ; then
+  wait_ssh ${ipa_mgmt_ip} ${ssh_private_key}
+  scp -r $ssh_opts rhosp-environment.sh $(dirname $my_dir) root@${ipa_mgmt_ip}:
+  rsync -a -e "ssh -i $ssh_private_key $ssh_opts" rhosp-environment.sh $tf_dir root@${ipa_mgmt_ip}:
+fi
