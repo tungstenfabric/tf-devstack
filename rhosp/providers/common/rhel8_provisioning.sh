30ad1e73d1e286bcb9008b34cb809301d9790f66
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I9cc0b98a6284d0b3653f2061461a58371d684a2a
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/providers/common/rhel8_provisioning.sh
+++ rhosp/providers/common/rhel8_provisioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-dnf update -y
-dnf install -y --allowerasing chrony wget yum-utils vim iproute jq curl bind-utils network-scripts net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
+sudo dnf update -y
+sudo dnf install -y --allowerasing chrony wget yum-utils vim iproute jq curl bind-utils network-scripts net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
 
-systemctl start chronyd
+sudo systemctl start chronyd
