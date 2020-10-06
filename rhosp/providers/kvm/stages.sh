7660f775a8ea03b6a7f232a5253f75964fe98909
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: If62e6a310fb8ab1e7ddcd77610902601c21861f5
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/providers/kvm/stages.sh
+++ rhosp/providers/kvm/stages.sh
@@ -1,134 +1,53 @@
-#Stages for KVM deployment (it's part of run.sh)
-
-source $my_dir/providers/kvm/virsh_functions
-
 
 function provisioning() {
-    cd $my_dir/providers/kvm
-    sudo -E ./01_create_env.sh
-    wait_ssh ${mgmt_ip} ${ssh_private_key}
+    cd $WORKSPACE
+    $my_dir/providers/kvm/01_create_env.sh
+    wait_ssh ${instance_ip} ${ssh_private_key}
     if [[ "$USE_PREDEPLOYED_NODES" == false ]]; then
-        sudo ./02_collecting_node_information.sh
+        $my_dir/providers/kvm/02_collecting_node_information.sh
     fi
+    scp $ssh_opts ${ssh_private_key} ${ssh_public_key} root@${instance_ip}:./.ssh/
+    scp $ssh_opts ${ssh_private_key} ${ssh_public_key} stack@${instance_ip}:./.ssh/
+    scp -r $ssh_opts rhosp-environment.sh instackenv.json $(dirname $my_dir) stack@${instance_ip}:
     if [[ -n "$ENABLE_TLS" ]] ; then
         wait_ssh ${ipa_mgmt_ip} ${ssh_private_key}
+        scp -r $ssh_opts rhosp-environment.sh $(dirname $my_dir) root@${ipa_mgmt_ip}:
     fi
+}
+
+function _run()
+{
+    ssh $ssh_opts stack@${instance_ip} ./tf-devstack/rhosp/run.sh $@
 }
 
 function machines() {
-    cd $my_dir
-    scp -r $ssh_opts ~/rhosp-environment.sh ~/instackenv.json $(dirname $my_dir) stack@${mgmt_ip}:
-    cat <<EOF | ssh $ssh_opts stack@${mgmt_ip}
-sudo ./tf-devstack/rhosp/undercloud/00_provision.sh
-EOF
-    if [[ -n "$ENABLE_TLS" ]] ; then
-        scp -r $ssh_opts ~/rhosp-environment.sh $(dirname $my_dir) root@${ipa_mgmt_ip}:
-        cat <<EOF | ssh $ssh_opts root@${ipa_mgmt_ip}
-source ./rhosp-environment.sh
-./tf-devstack/rhosp/providers/common/rhel_provisioning.sh
-./tf-devstack/rhosp/ipa/freeipa_setup.sh
-EOF
-    fi
+    _run machines
 }
 
 function undercloud() {
-    cd $my_dir
-    local otp=""
-    if [[ -n "$ENABLE_TLS" ]] ; then
-        otp=$(ssh $ssh_opts root@${ipa_mgmt_ip} cat ./undercloud_otp)
-    fi
-    cat <<EOF | ssh $ssh_opts stack@${mgmt_ip}
-set -x
-export OTP_PASSWORD=$otp
-sudo ./tf-devstack/rhosp/undercloud/01_deploy_as_root.sh
-./tf-devstack/rhosp/undercloud/02_deploy_as_stack.sh
-./tf-devstack/rhosp/undercloud/${RHOSP_VERSION}_configure_registries_undercloud.sh
-EOF
+    _run undercloud
 }
 
-#Overcloud nodes provisioning
 function overcloud() {
-    cd $my_dir
-    if [[ "$USE_PREDEPLOYED_NODES" == false ]]; then
-        ssh $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/01_extract_overcloud_images.sh
-        #Checking vbmc statuses and fix 'down'
-        local vm
-        for vm in $(vbmc list -f value -c 'Domain name' -c Status | grep down | awk '{print $1}'); do
-            vbmc start ${vm}
-        done
-        if ! ssh $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/03_node_introspection.sh ; then
-            echo "ERROR: failed ssh $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/03_node_introspection.sh"
-            exit 1
-        fi
-        return
-    fi
-
-    # Predeployed nodes
-    #
-    #Copying keypair to the undercloud
-    scp $ssh_opts $ssh_private_key $ssh_public_key stack@${mgmt_ip}:.ssh/
-    #start overcloud VMs
-    for domain in $(virsh list --name --all | grep $RHOSP_VERSION-overcloud-$DEPLOY_POSTFIX) ; do
-        virsh start $domain
-    done
-    # copy prepare scripts
-    local ip
-    for ip in $overcloud_cont_prov_ip $overcloud_compute_prov_ip $overcloud_ctrlcont_prov_ip; do
-        wait_ssh ${ip} ${ssh_private_key}
-        scp $ssh_opts ~/rhosp-environment.sh ../common/collect_logs.sh ../common/common.sh \
-        ../common/create_docker_config.sh ../common/jinja2_render.py \
-        providers/common/* overcloud/03_setup_predeployed_nodes.sh stack@$ip:
-        ssh $ssh_opts $SSH_USER@$ip mkdir -p ./files
-        scp $ssh_opts ../common/files/docker_daemon.json.j2 $SSH_USER@$ip:files/
-    done
-    #parallel ssh
-    local jobs=''
-    for ip in $overcloud_cont_prov_ip $overcloud_compute_prov_ip $overcloud_ctrlcont_prov_ip; do
-        ssh $ssh_opts stack@${ip} sudo ./03_setup_predeployed_nodes.sh &
-        jobs+=" $!"
-    done
-    echo Parallel pre-instatallation overcloud nodes. pids: $jobs. Waiting...
-    local res=0
-    local i
-    for i in $jobs ; do
-        command wait $i || {
-            echo "ERROR: job $i failed"
-            res=1
-        }
-    done
-    if [[ "${res}" == 1 ]]; then
-        echo "ERROR: errors appeared during overcloud nodes pre-installation."
-        exit 1
-    fi
+    _run overcloud
 }
 
-# TODO:
-#   - move flavor into overcloud stage
-#   - split containers preparation into openstack and contrail parts
-#     and move openstack part into overcloud stage
-#Overcloud stage w/o deploy for debug and customizations pruposes
 function tf_no_deploy() {
-    cd $my_dir
-    ssh  $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/02_manage_overcloud_flavors.sh
-    ssh  $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/04_prepare_heat_templates.sh
-    ssh  $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/05_prepare_containers.sh
+    _run tf_no_deploy
 }
 
-#Overcloud stage
 function tf() {
-    cd $my_dir
-    ssh  $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/02_manage_overcloud_flavors.sh
-    ssh  $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/04_prepare_heat_templates.sh
-    ssh  $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/05_prepare_containers.sh
-    ssh  $ssh_opts stack@${mgmt_ip} /home/stack/tf-devstack/rhosp/overcloud/06_deploy_overcloud.sh
+    _run tf
 }
 
-# This is_active function is called in wait stage defined in common/stages.sh
 function is_active() {
     return 0
 }
 
+function logs() {
+    _run logs
+}
+
 function collect_deployment_env() {
-    # no additinal info is needed
-    :
+    ;
 }
