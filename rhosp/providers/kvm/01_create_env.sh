597b7fd23b0d6dc8628f8e75c89f4462ff66225b
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: If62e6a310fb8ab1e7ddcd77610902601c21861f5
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/providers/kvm/01_create_env.sh
+++ rhosp/providers/kvm/01_create_env.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -ex
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run by root"
-   exit 1
-fi
-
-
-# VBMC base port for IPMI management
-VBMC_PORT_BASE=16000
 
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
+cd
+source rhosp-environment.sh
+source $my_dir/virsh_functions
+
+# VBMC base port for IPMI management
+VBMC_PORT_BASE=16000
 
 OS_MEM=${OS_MEM:-8192}
 CTRL_MEM=${CTRL_MEM:-8192}
@@ -20,9 +18,6 @@ IPA_MEM=${COMP_MEM:-16384}
 
 vm_disk_size=${vm_disk_size:-30G}
 net_driver=${net_driver:-virtio}
-
-source "/home/$SUDO_USER/rhosp-environment.sh"
-source "$my_dir/virsh_functions"
 
 if [[ $RHEL_VERSION == 'rhel8' ]]; then
    rhel_version_libvirt='rhl8.0'
@@ -35,7 +30,7 @@ assert_env_exists $undercloud_vmname
 
 # create networks and setup DHCP rules
 create_network_dhcp $NET_NAME_MGMT $mgmt_subnet $BRIDGE_NAME_MGMT
-update_network_dhcp $NET_NAME_MGMT $undercloud_vmname $undercloud_mgmt_mac $mgmt_ip
+update_network_dhcp $NET_NAME_MGMT $undercloud_vmname $undercloud_mgmt_mac $instance_ip
 if [[ -n "$ENABLE_TLS" ]] ; then
   update_network_dhcp $NET_NAME_MGMT $ipa_vmname $ipa_mgmt_mac $ipa_mgmt_ip
 fi
@@ -67,39 +62,17 @@ function define_overcloud_vms() {
   start_vbmc $vbmc_port $vm_name $mgmt_gateway $IPMI_USER $IPMI_PASSWORD
 }
 
-function define_overcloud_vms_without_vbmc() {
-  local name=$1
-  local mem=$2
-  local mac=$3
-  local ip=$4
-  local vcpu=${5:-2}
-  local vol_name=$name
-  #create_root_volume $vol_name
-  local vm_name="$vol_name"
-  cp -p $BASE_IMAGE $pool_path/$vol_name.qcow2
-  image_customize $pool_path/$vol_name.qcow2 $vm_name $ssh_public_key $domain $ip
-  update_network_dhcp $NET_NAME_PROV $vm_name $mac $ip
-  define_machine $vm_name $vcpu $mem $rhel_version_libvirt $NET_NAME_PROV/$mac "${pool_path}/${vol_name}.qcow2"
-}
-
-# just define overcloud machines
-if [[ "$USE_PREDEPLOYED_NODES" == false ]]; then
-  vbmc_port=$VBMC_PORT_BASE
-  for i in $(echo $overcloud_cont_instance | sed 's/,/ /g') ; do
-    define_overcloud_vms $i $OS_MEM $vbmc_port 4
-    (( vbmc_port+=1 ))
-  done
-  for i in $(echo $overcloud_compute_instance | sed 's/,/ /g') ; do
-    define_overcloud_vms $i $COMP_MEM $vbmc_port 4
-    (( vbmc_port+=1 ))
-  done
-  define_overcloud_vms $overcloud_ctrlcont_instance $CTRL_MEM $vbmc_port 4
+vbmc_port=$VBMC_PORT_BASE
+for i in $(echo $overcloud_cont_instance | sed 's/,/ /g') ; do
+  define_overcloud_vms $i $OS_MEM $vbmc_port 4
   (( vbmc_port+=1 ))
-else
-  define_overcloud_vms_without_vbmc $overcloud_cont_instance $OS_MEM $overcloud_cont_prov_mac $overcloud_cont_prov_ip 4
-  define_overcloud_vms_without_vbmc $overcloud_compute_instance $COMP_MEM $overcloud_compute_prov_mac $overcloud_compute_prov_ip 4
-  define_overcloud_vms_without_vbmc $overcloud_ctrlcont_instance $CTRL_MEM $overcloud_ctrlcont_prov_mac $overcloud_ctrlcont_prov_ip 4
-fi
+done
+for i in $(echo $overcloud_compute_instance | sed 's/,/ /g') ; do
+  define_overcloud_vms $i $COMP_MEM $vbmc_port 4
+  (( vbmc_port+=1 ))
+done
+define_overcloud_vms $overcloud_ctrlcont_instance $CTRL_MEM $vbmc_port 4
+(( vbmc_port+=1 ))
 
 # copy image for undercloud and resize them
 cp -p $BASE_IMAGE $pool_path/$undercloud_vm_volume
@@ -111,8 +84,8 @@ if [[ -n "$ENABLE_TLS" ]] ; then
 fi
 
 #check that nbd kernel module is loaded
-if ! lsmod |grep '^nbd ' ; then
-  modprobe nbd max_part=8
+if ! sudo lsmod |grep '^nbd ' ; then
+  sudo modprobe nbd max_part=8
 fi
 
 function _start_vm() {
