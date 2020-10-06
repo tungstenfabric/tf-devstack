5e8bcf436e01009ec1dc736fc99d379896970cfd
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: If62e6a310fb8ab1e7ddcd77610902601c21861f5
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/overcloud/05_prepare_containers.sh
+++ rhosp/overcloud/05_prepare_containers.sh
@@ -4,8 +4,8 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 cd
-source rhosp-environment.sh
 source stackrc
+source rhosp-environment.sh
 
 #Specific part
 source $my_dir/${RHOSP_VERSION}_prepare_containers.sh
