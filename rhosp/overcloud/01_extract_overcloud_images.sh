a4a6609a5e27e1301f4c1c3f368c259f40541859
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: If62e6a310fb8ab1e7ddcd77610902601c21861f5
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/overcloud/01_extract_overcloud_images.sh
+++ rhosp/overcloud/01_extract_overcloud_images.sh
@@ -4,15 +4,17 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 cd
-source rhosp-environment.sh
 source stackrc
+source rhosp-environment.sh
 
 mkdir -p images
 pushd images
-for i in /usr/share/rhosp-director-images/overcloud-full-latest.tar /usr/share/rhosp-director-images/ironic-python-agent-latest.tar; do
-  tar -xvf $i;
-done
-popd
 
-openstack overcloud image upload --image-path images
+for i in /usr/share/rhosp-director-images/overcloud-full-latest.tar /usr/share/rhosp-director-images/ironic-python-agent-latest.tar; do
+  tar -xvf $i
+done
+
+openstack overcloud image upload --image-path .
 openstack image list
+
+popd
