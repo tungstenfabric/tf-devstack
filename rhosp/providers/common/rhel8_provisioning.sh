4737c447af0e4ffdf8d9eca1a6a8106d4cd57e83
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I47146e6f0fa6834bf99487a61034fda1c9c3e251
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/providers/common/rhel8_provisioning.sh
+++ rhosp/providers/common/rhel8_provisioning.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -e
 
-dnf update -y
-dnf install -y --allowerasing chrony wget yum-utils vim iproute jq curl bind-utils network-scripts net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
+#removing docker-ce package to avoid conflicts with podman
+sudo dnf remove -y docker-ce-cli || true
 
-systemctl start chronyd
+if [[ "$ENABLE_RHEL_REGISTRATION" == 'true' ]] ; then
+  # fix module otherwise upstream usage leads to packages conflicts
+  sudo dnf module disable -y container-tools:rhel8
+  sudo dnf module enable -y container-tools:2.0
+  sudo dnf distro-sync -y 
+fi
+
+sudo dnf update -y
+sudo dnf install -y --allowerasing chrony wget yum-utils vim iproute jq curl bind-utils network-scripts net-tools tmux createrepo bind-utils sshpass python36 python3-pip python3-virtualenv podman
+
+sudo systemctl start chronyd
