3feea2bccea39b0f129baf1447e44fbd5f043423
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I47146e6f0fa6834bf99487a61034fda1c9c3e251
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/providers/kvm/02_collecting_node_information.sh
+++ rhosp/providers/kvm/02_collecting_node_information.sh
@@ -1,36 +1,28 @@
 #!/bin/bash -ex
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run by root. Please use sudo"
-   exit 1
-fi
-
-
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-#BASE_IMAGE="/home/ubuntu/rhel-7.7.qcow2"
-
-source "/home/$SUDO_USER/rhosp-environment.sh"
-cd $my_dir
+source rhosp-environment.sh
+source $my_dir/../../../common/common.sh
+source $my_dir/virsh_functions
 
 # collect MAC addresses of overcloud machines
 function get_macs() {
   local name=$1
   truncate -s 0 /tmp/nodes-$name.txt
-  virsh domiflist $name | awk '$3 ~ "prov" {print $5};'
+  sudo virsh domiflist $name | awk '$3 ~ "prov" {print $5};'
 }
 
 function get_vbmc_ip() {
   local name=$1
-  vbmc list | grep $name | awk -F\| '{print $4}'
+  call_vbmc list | grep $name | awk -F\| '{print $4}'
 }
 
 function get_vbmc_port() {
   local name=$1
-  vbmc list | grep $name | awk -F\| '{print $5}'
+  call_vbmc list | grep $name | awk -F\| '{print $5}'
 }
-
 
 function define_machine() {
   local caps=$1
@@ -102,8 +94,6 @@ cat << EOF >> instackenv.json
   ]
 }
 EOF
-
-mv instackenv.json /home/$SUDO_USER/
 
 # check this json (it's optional)
 # curl --silent -O https://raw.githubusercontent.com/rthallisey/clapper/master/instackenv-validator.py
