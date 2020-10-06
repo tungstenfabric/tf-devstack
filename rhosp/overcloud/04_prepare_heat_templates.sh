38232228766cefd2595f85b98cca74fd8cd0614e
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I47146e6f0fa6834bf99487a61034fda1c9c3e251
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/overcloud/04_prepare_heat_templates.sh
+++ rhosp/overcloud/04_prepare_heat_templates.sh
@@ -4,17 +4,23 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 cd
-source rhosp-environment.sh
 source stackrc
+source rhosp-environment.sh
 
-source "$my_dir/../../common/functions.sh"
-source "$my_dir/../providers/common/functions.sh"
+source $my_dir/../../common/common.sh
+source $my_dir/../../common/functions.sh
+source $my_dir/../providers/common/functions.sh
 
-export OPENSTACK_VERSION=${OPENSTACK_VERSION:-'queens'}
-export CONTRAIL_CONTAINER_TAG=${CONTRAIL_CONTAINER_TAG:-"latest"}
-export contrail_dpdk_driver=${contrail_dpdk_driver:-"uio_pci_generic"}
+function _get_count()
+{
+   if [[ -n "$1" ]] ; then
+      echo $(( $(echo $1 | grep -o ',' | wc -l) + 1))
+   else
+      echo 0
+   fi
+}
 
-if [[ "$backend_storage" == "rbd" ]] ; then
+if [[ -n "$overcloud_ceph_instance" ]] ; then
    export glance_backend_storage="rbd"
 else
    export glance_backend_storage="file"
@@ -26,13 +32,35 @@ else
    export overcloud_nameservers="[ \"8.8.8.8\", \"8.8.4.4\" ]"
 fi
 
-rhosp_branch="stable/${OPENSTACK_VERSION}"
-tf_rhosp_image="tf-tripleo-heat-templates-src"
+if (( $(_get_count $overcloud_cont_instance) > 1 )) ; then
+   export enable_galera=true
+else
+   export enable_galera=false
+fi
+
+export undercloud_registry=${prov_ip}:8787
+export undercloud_registry_contrail=$undercloud_registry
+ns=$(echo ${CONTAINER_REGISTRY:-'docker.io/tungstenfabric'} | cut -s -d '/' -f2-)
+[ -n "$ns" ] && undercloud_registry_contrail+="/$ns"
+if [[ "$USE_PREDEPLOYED_NODES" == true ]]; then
+   #Explicitly set to prevent the use of a network interface gateway
+  export vrouter_gateway_parameter="VROUTER_GATEWAY: ${prov_ip}"
+fi
+if [[ "$ENABLE_RHEL_REGISTRATION" == false ]]; then
+   export RHEL_REG_METHOD="disable"
+else
+   export RHEL_REG_METHOD="portal"
+   #Getting orgID
+   export RHEL_ORG_ID=$(sudo subscription-manager identity | grep "org ID" | sed -e 's/^.*: //')
+fi
+
+export SSH_PRIVATE_KEY=`while read l ; do echo "      $l" ; done < .ssh/id_rsa`
+export SSH_PUBLIC_KEY=`while read l ; do echo "      $l" ; done < .ssh/id_rsa.pub`
 
 cd
 rm -rf tripleo-heat-templates contrail-tripleo-heat-templates
 cp -r /usr/share/openstack-tripleo-heat-templates/ tripleo-heat-templates
-fetch_deployer_no_docker ${tf_rhosp_image} contrail-tripleo-heat-templates \
+fetch_deployer_no_docker "tf-tripleo-heat-templates-src" contrail-tripleo-heat-templates \
 || git clone https://github.com/tungstenfabric/tf-tripleo-heat-templates contrail-tripleo-heat-templates
 
 if [[ ! -d contrail-tripleo-heat-templates ]] ; then
@@ -40,65 +68,47 @@ if [[ ! -d contrail-tripleo-heat-templates ]] ; then
    exit 1
 fi
 pushd contrail-tripleo-heat-templates
+rhosp_branch="stable/${OPENSTACK_VERSION}"
 git checkout ${rhosp_branch}
 if [[ $? != 0 ]] ; then
    echo "ERROR: Checkout to ${rhosp_branch} is finished with error"
    exit 1
 fi
 popd
-
 cp -r contrail-tripleo-heat-templates/* tripleo-heat-templates
 
-export SSH_PRIVATE_KEY=`while read l ; do echo "      $l" ; done < .ssh/id_rsa`
-export SSH_PUBLIC_KEY=`while read l ; do echo "      $l" ; done < .ssh/id_rsa.pub`
-
 cat $my_dir/misc_opts.yaml.template | envsubst > misc_opts.yaml
-if [[ "$PROVIDER" == "bmc" ]]; then
-  cat <<EOF >> misc_opts.yaml
-  ControllerCount: 3
-  ContrailControllerCount: 3
-  ComputeCount: 1
-  ContrailSriovCount: 1
-  ContrailDpdkCount: 1
-  ContrailDpdkDriver: ${contrail_dpdk_driver}
-  node_admin_username: ${SSH_USER}
-  CephStorageCount: 3
+
+cat <<EOF >> misc_opts.yaml
+  ControllerCount: $(_get_count $overcloud_cont_instance)
+  ContrailControllerCount: $(_get_count $overcloud_ctrlcont_instance)
+  ComputeCount: $(_get_count $overcloud_compute_instance)
+  ContrailDpdkCount: $(_get_count $overcloud_dpdk_instance)
+  ContrailSriovCount: $(_get_count $overcloud_sriov_instance)
+  CephStorageCount: $(_get_count $overcloud_ceph_instance)
   CephDefaultPoolSize: 2
   CephPoolDefaultPgNum: 8
   ManilaCephFSDataPoolPGNum: 8
   ManilaCephFSMetadataPoolPGNum: 8
-  ContrailVrouterHugepages1GB: 6
 EOF
-  cat tripleo-heat-templates/network/config/contrail/contrail-dpdk-nic-config-single.yaml > \
-      tripleo-heat-templates/network/config/contrail/contrail-dpdk-nic-config.yaml
-else
-   if [[ -n "$overcloud_cont_instance" ]] ; then
-      count=$(( $(echo $overcloud_cont_instance | grep -o ',' | wc -l) + 1))
-      echo "  ControllerCount: $count" >> misc_opts.yaml
-   fi
-   if [[ -n "$overcloud_compute_instance" ]] ; then
-      count=$(( $(echo $overcloud_compute_instance | grep -o ',' | wc -l) + 1))
-      echo "  ComputeCount: $count" >> misc_opts.yaml
-   fi
+
+if [[ "$CONTRAIL_CONTAINER_TAG" =~ 'r1912' ]] ; then
+  # Disable kernel vrouter hugepages for 1912 as it is not used there
+  sed -i '/ContrailVrouterHugepages/d' tripleo-heat-templates/environments/contrail/contrail-services.yaml
+  cat <<EOF >> misc_opts.yaml
+  ContrailVrouterHugepages1GB: '0'
+  ContrailVrouterHugepages2MB: '128'
+EOF
 fi
 
-#Creating file for overcloud rhel registration (rhosp version specific)
-if [[ "$ENABLE_RHEL_REGISTRATION" == false ]]; then
-   export RHEL_REG_METHOD="disable"
-else
-   export RHEL_REG_METHOD="portal"
+if [ -n "$vrouter_huge_pages_1g" ] ; then
+   # enable hugepages: it is set explicitely on bmc setup as nova needs hugepages
+   # because tf-test use such flavor if dpdk nodes are presented 
+   echo "  ContrailVrouterHugepages1GB: $vrouter_huge_pages_1g" >> misc_opts.yaml
 fi
+
+#Creating rhosp specific contrail-parameters.yaml
 source $my_dir/${RHOSP_VERSION}_prepare_heat_templates.sh
-
-#Creating contrail-parameters.yaml
-export undercloud_registry=${prov_ip}:8787
-export undercloud_registry_contrail=$undercloud_registry
-ns=$(echo ${CONTAINER_REGISTRY:-'docker.io/tungstenfabric'} | cut -s -d '/' -f2-)
-[ -n "$ns" ] && undercloud_registry_contrail+="/$ns"
-#Explicitly set to prevent the use of a network interface gateway
-if [[ "$USE_PREDEPLOYED_NODES" == true ]]; then
-  export vrouter_gateway_parameter="VROUTER_GATEWAY: ${prov_ip}"
-fi
 cat $my_dir/${RHOSP_VERSION}_contrail-parameters.yaml.template | envsubst > contrail-parameters.yaml
 
 #Changing tripleo-heat-templates/roles_data_contrail_aio.yaml
--- rhosp/overcloud/06_deploy_overcloud.sh
+++ rhosp/overcloud/06_deploy_overcloud.sh
@@ -4,9 +4,10 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 cd
-source rhosp-environment.sh
 source stackrc
-source "$my_dir/../providers/common/functions.sh"
+source rhosp-environment.sh
+source $my_dir/../../common/common.sh
+source $my_dir/../providers/common/functions.sh
 
 #Specific part of deployment
 source $my_dir/${RHOSP_VERSION}_deploy_overcloud.sh
