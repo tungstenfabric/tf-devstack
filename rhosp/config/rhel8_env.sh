bfec64f261be6a29559f76311ef3cdbdeba9108b
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I78f60a22c0e4d7f6c2d0f8c809514c323eaf7fe1
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/config/rhel8_env.sh
+++ rhosp/config/rhel8_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #List of yum repositories for RHOSP16 on RHEL8
-export RHEL_REPOS="rhel-8-for-x86_64-baseos-rpms rhel-8-for-x86_64-appstream-rpms rhel-8-for-x86_64-highavailability-rpms ansible-2-for-rhel-8-x86_64-rpms satellite-tools-6.5-for-rhel-8-x86_64-rpms openstack-16.1-for-rhel-8-x86_64-rpms fast-datapath-for-rhel-8-x86_64-rpms"
+export RHEL_REPOS="rhel-8-for-x86_64-baseos-rpms,rhel-8-for-x86_64-appstream-rpms,rhel-8-for-x86_64-highavailability-rpms,ansible-2-for-rhel-8-x86_64-rpms,satellite-tools-6.5-for-rhel-8-x86_64-rpms,openstack-16.1-for-rhel-8-x86_64-rpms,fast-datapath-for-rhel-8-x86_64-rpms,rhceph-4-tools-for-rhel-8-x86_64-rpms"
 
