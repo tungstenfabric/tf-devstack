8a0c3d20be7efb759d168d5d09d0e283443edec9
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I9cc0b98a6284d0b3653f2061461a58371d684a2a
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

---
+++ rhosp/undercloud/rhosp13_undercloud_deploy.sh
@@ -0,0 +1,19 @@
+#RHEL7 undercloud install
+
+if [[ "${ENABLE_RHEL_REGISTRATION}" == 'true' ]] ; then
+   sudo yum-config-manager --enable rhelosp-rhel-7-server-opt
+fi
+
+sudo yum -y install python-tripleoclient python-rdomanager-oscplugin iproute rhosp-director-images ceph-ansible
+
+cat $my_dir/${RHOSP_VERSION}_undercloud.conf.template | envsubst >~/undercloud.conf
+echo "INFO: undercloud.conf"
+cat ~/undercloud.conf
+
+openstack undercloud install
+
+#Adding user to group docker
+user=$(whoami)
+sudo usermod -a -G docker $user
+
+echo User "$user" has been added to group "docker". Please relogin
