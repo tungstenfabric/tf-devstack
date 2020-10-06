283c43a6e1e2778a84c717e10a34d056e5a7979a
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Depends-On: I6aa7dd807cbe47e1196520c0d3957020757583ae
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/config/common.sh
+++ rhosp/config/common.sh
@@ -2,9 +2,6 @@
 
 export RHEL_POOL_ID=8a85f99970453685017057d235142b3b
 
-export undercloud_local_interface=${undercloud_local_interface:-"eth1"}
-export keystone_admin_api_network=${keystone_admin_api_network:-"ctlplane"}
-
 state="$(set +o)"
 [[ "$-" =~ e ]] && state+="; set -e"
 
@@ -13,3 +10,6 @@ set +x
 export RHEL_PASSWORD=$RHEL_PASSWORD
 export RHEL_USER=$RHEL_USER
 eval "$state"
+
+export undercloud_local_interface=${undercloud_local_interface:-"eth1"}
+export contrail_dpdk_driver=${contrail_dpdk_driver:-"uio_pci_generic"}
