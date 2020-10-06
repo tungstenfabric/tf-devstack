01a3fe1a659e70d075803bfd14bb2f367fa1aa25
alexey-mr <alexey.morlang@gmail.com>
2020-10-06 19:49:47 +0300

    rhosp: refactoring to unify stages code between providers
    Change-Id: Ic49f59f1c8daf0370f0d81bb5c6c20f055249887

--- rhosp/providers/common/rhel7_provisioning.sh
+++ rhosp/providers/common/rhel7_provisioning.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
-yum update -y
-yum install -y ntp wget yum-utils vim iproute jq curl bind-utils bridge-utils net-tools python-heat-agent*
+sudo yum update -y
+sudo yum install -y ntp wget yum-utils vim iproute jq curl bind-utils bridge-utils net-tools python-heat-agent*
 
-chkconfig ntpd on
-service ntpd start
+sudo chkconfig ntpd on
+sudo service ntpd start
 
 # install pip for future run of OS checks
-curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/get-pip.py | python
-pip install -q virtualenv docker
+curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/get-pip.py | sudo python
+sudo pip install -q virtualenv docker
 
 # install pyton3 after pip
-yum install -y python3
-curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/get-pip.py | python3
-python3 -m pip install -q six pyyaml
+sudo yum install -y python3
+curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/get-pip.py | sudo python3
+sudo python3 -m pip install -q six pyyaml
