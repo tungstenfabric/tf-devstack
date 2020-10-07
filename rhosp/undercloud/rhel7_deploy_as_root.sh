29dcadc464da9c8e3363b661e38219012fb61e1a
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-10-07 14:25:44 +0300

    Enable Ceph backend for rhosp13-bmc
    Partial-Jira-Bug: CEM-19466
    Change-Id: Ibc45d1537578890787118a07d205451282e5a52c
    Depends-On: Ie968fb75c1a6c72429f65d2fc3bd0db885c06d81
    Depends-On: Ie95b96bd86fced6c895065f8046b5c6a08dffbca

--- rhosp/undercloud/rhel7_deploy_as_root.sh
+++ rhosp/undercloud/rhel7_deploy_as_root.sh
@@ -4,3 +4,9 @@ if [[ "${ENABLE_RHEL_REGISTRATION}" == 'true' ]] ; then
 fi
 
 yum -y install python-tripleoclient python-rdomanager-oscplugin iproute rhosp-director-images
+
+# ceph-ansible v3.1 contained in rhel-7-server-openstack-13-rpms requires ansible v2.4
+# ceph-ansible v3.2 contained in rhel-7-server-rhceph-3-tools-rpms requires ansible v2.6 
+if [[ "$backend_storage" == "rbd" ]] ; then
+   yum -y install ceph-ansible
+fi
