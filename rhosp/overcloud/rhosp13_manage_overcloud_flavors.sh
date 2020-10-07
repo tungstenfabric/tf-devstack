17907f2fdde973f04a7f0a83b0d2ba2fbd60fc07
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-10-07 14:25:44 +0300

    Enable Ceph backend for rhosp13-bmc
    Partial-Jira-Bug: CEM-19466
    Change-Id: Ibc45d1537578890787118a07d205451282e5a52c
    Depends-On: Ie968fb75c1a6c72429f65d2fc3bd0db885c06d81

--- rhosp/overcloud/rhosp13_manage_overcloud_flavors.sh
+++ rhosp/overcloud/rhosp13_manage_overcloud_flavors.sh
@@ -3,7 +3,12 @@ function create_flavor() {
   local name=$1
   local profile=${2:-''}
   openstack flavor create --id auto --ram 1000 --disk 29 --vcpus 2 $name
-  openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" $name
+  openstack flavor set --property "cpu_arch"="x86_64" \
+                       --property "capabilities:boot_option"="local" \
+                       --property "resources:VCPU"="0" \
+                       --property "resources:MEMORY_MB"="0" \
+                       --property "resources:DISK_GB"="0" \
+                       $name
   if [[ -n "$profile" ]] ; then
     openstack flavor set --property "capabilities:profile"="${profile}" $name
   else
@@ -16,3 +21,4 @@ create_flavor 'compute' 'compute'
 create_flavor 'contrail-controller' 'contrail-controller'
 create_flavor 'compute-dpdk' 'compute-dpdk'
 create_flavor 'compute-sriov' 'compute-sriov'
+create_flavor 'ceph-storage' 'ceph-storage'
