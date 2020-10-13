eee2d64f3e550676f599740aeb0f0986a8dc283b
Kirill Egorov <kegorov@progmaticlab.com>
2020-10-14 00:10:35 +0300

    Resolve cleanup for juju deployer
    partial-jira-bug: CEM-19763
    Change-Id: I58bb614ef783a96afe62457927e5c8ca0b4d74e0

--- juju/cleanup.sh
+++ juju/cleanup.sh
@@ -1,47 +1,102 @@
-#!/bin/bash
-
-set -o errexit
-
+#!/bin/bash -e
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
-source "$my_dir/../common/common.sh"
-source "$my_dir/../common/functions.sh"
+source "${my_dir}/../common/common.sh"
+source "${my_dir}/../common/functions.sh"
+
 
 export CLOUD=${CLOUD:-manual}  # aws | maas | manual
+[[ $CLOUD == 'maas' ]] && echo "INFO: This cleanup is not intended for maas" && exit
 
-rm -rf ~/.tf/.stages
 
-echo "Destroying tf-$CLOUD-controller controller, machines, applications and data."
-if [ -n "$(juju status --format json | jq '.applications' | jq -r 'keys[]')" ]; then
-    juju status --format json | jq '.applications' | jq -r 'keys[]' | xargs -n 1 juju remove-application --force
-fi
+echo "INFO: Removing all juju applications"
 
-function no_apps_left() {
-    if [ -z "$(juju status --format json | jq '.applications' | jq -r 'keys[]')" ]; then
-        return 0
-    fi
-    return 1
+function get_app_names() {
+    juju status --format json | jq '.applications' | jq -r 'keys[]'
 }
 
-wait_cmd_success "no_apps_left"
+function is_app_removed() {
+    local app_name=$1
+    get_app_names | grep -xq "$app_name" && return 1 || return 0
+}
 
-juju destroy-controller -y --destroy-all-models tf-$CLOUD-controller
+function are_all_apps_removed() {
+    [[ -z "$(get_app_names)" ]] && return 0 || return 1
+}
 
-[[ $CLOUD == 'maas' ]] && echo "Cleanup is over." && exit
+# It's important to remove applications sequentially because this ensures that
+# `stop` hooks work properly
+for app_name in $(get_app_names)
+do
+    juju remove-application "$app_name"
+    wait_cmd_success "is_app_removed $app_name" 1 60 || echo "WARNING: Application $app_name wasn't removed"
+done
 
-echo "Clean up the juju-controller-node."
-sudo rm -rf /var/lib/juju
-sudo rm -rf /lib/systemd/system/juju*
-sudo rm -rf /run/systemd/units/invocation:juju*
-sudo rm -rf /etc/systemd/system/juju*
-sudo rm -rf /etc/contrail/
-sudo rm -rf /etc/docker
+if [[ -n "$(get_app_names)" ]]; then
+    echo "WARNING: Some of juju applications weren't removed"
+    echo "INFO: Force removing remaining juju applications"
+    get_app_names | xargs -n 1 juju remove-application --force
+    wait_cmd_success "are_all_apps_removed" || echo "WARNING: Some of applications weren't removed"
+fi
+
+
+echo "INFO: Destroy controller"
+juju destroy-controller -y --destroy-all-models "tf-${CLOUD}-controller"
+
+# On start controller juju changes configuration of fan, but after destroying it
+# doesn't return everything to its place
+echo "INFO: Destroy fan config"
+sudo fanctl config list | while read -r addrs
+do
+    addrs=($addrs)
+    fanatic disable-fan -u ${addrs[0]} -o ${addrs[1]}
+done
+
+
+if [[ -n "$(sudo docker ps -q)" ]]; then
+    # It doesn't run, if juju applications were removed successfully
+    echo "WARNING: Some of docker containers weren't removed"
+    sudo docker ps
+    echo "INFO: Force removing remaining docker containers"
+    sudo docker stop $(sudo docker ps -aq)
+    sudo docker rm $(sudo docker ps -aq)
+fi
+
+
+echo "INFO: Remove docker, lxc, juju"
+remove_packages_cmd="\
+sudo apt purge -y lxd lxd-client ; \
+sudo apt purge -y docker-ce=18.06.3~ce~3-0~ubuntu ; \
+sudo apt -y autoremove ; \
+sudo snap remove --purge juju"
+# sudo apt purge -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# sudo apt purge -y jq dnsutils
+eval "$remove_packages_cmd"
+
+
+echo "INFO: Remove other artifacts"
+remove_files_cmd="\
+sudo rm -rf /var/lib/juju ; \
+sudo rm -rf /lib/systemd/system/juju* ; \
+sudo rm -rf /run/systemd/units/invocation:juju* ; \
+sudo rm -rf /etc/systemd/system/juju* ; \
+sudo rm -rf /etc/contrail/ ; \
+sudo rm -rf /etc/docker"
+eval "$remove_files_cmd"
+
 
 if [[ $CLOUD == 'manual' ]] && [[ -n "$CONTROLLER_NODES" || -n "$AGENT_NODES" ]]; then
-    echo "Clean other nodes." 
+    echo "INFO: Clean other nodes" 
     for machine in $CONTROLLER_NODES $AGENT_NODES ; do
-        ssh ubuntu@$machine "sudo rm -rf /var/lib/juju ; sudo rm -rf /lib/systemd/system/juju* ; sudo rm -rf /run/systemd/units/invocation:juju* ; sudo rm -rf /etc/systemd/system/juju* ; sudo rm -rf /etc/contrail/ ; sudo rm -rf /etc/docker"
+        ssh ubuntu@$machine "$remove_packages_cmd"
+        ssh ubuntu@$machine "$remove_files_cmd"
     done
 fi
 
-echo "Cleanup is over."
+
+rm -rf ~/.tf/.stages
+echo "INFO: Cleanup is over"
+
+
+
+
