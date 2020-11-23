d10ba01ad5f57f74ad04e8dd13d2dafa5422928d
alexey-mr <alexey.morlang@gmail.com>
2020-11-23 14:51:24 +0300

    [rhosp] support vexx ha env creation
    partial-jira-bug: CEM-19740
    Change-Id: Ib03b389828f68d8a194c4dc66763804677f31cb7

--- rhosp/providers/vexx/cleanup.sh
+++ rhosp/providers/vexx/cleanup.sh
@@ -5,7 +5,11 @@ if [[ -z ${OS_USERNAME+x}  && -z ${OS_PASSWORD+x} && -z ${OS_PROJECT_ID+x} ]]; t
    exit 1
 fi
 
-for i in ${overcloud_cont_instance} ${overcloud_ctrlcont_instance} ${overcloud_compute_instance} ${undercloud_instance} ; do
-   echo "Deleting server $i"
-   openstack server delete $i || true
+jobs=''
+for instance_name in ${overcloud_cont_instance//,/ } ${overcloud_compute_instance//,/ } ${overcloud_ctrlcont_instance//,/ } ${undercloud_instance}; do
+   echo "Deleting server $instance_name"
+   openstack server delete $instance_name &
+   job=+=" $!"
 done
+
+wait || true
