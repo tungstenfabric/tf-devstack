9f5086535bd77b639a61c535dbb6d8abe99c7f6d
alexey-mr <alexey.morlang@gmail.com>
2020-11-26 14:19:20 +0300

    [rhosp] added ipa support for vexx
    Change-Id: I97fc2732950fa4b24d9ff6b0fbeb4c1818a5dba0

--- rhosp/providers/common/rhel_provisioning.sh
+++ rhosp/providers/common/rhel_provisioning.sh
@@ -6,7 +6,7 @@ my_dir="$(dirname $my_file)"
 cd
 source rhosp-environment.sh
 source $my_dir/../../../common/common.sh
-source $my_dir/../common/common.sh
+source $my_dir/../../../common/functions.sh
 source $my_dir/common.sh
 
 attach_opts='--auto'
@@ -17,32 +17,43 @@ fi
 cd
 sudo getenforce
 sudo cat /etc/selinux/config
-if [[ "${ENABLE_RHEL_REGISTRATION}" == 'true' ]] ; then
-   set +x
-   register_opts=''
-   [ -n "$RHEL_USER" ] && register_opts+=" --username $RHEL_USER"
-   [ -n "$RHEL_PASSWORD" ] && register_opts+=" --password $RHEL_PASSWORD"
 
-   for i in {1..10} ; do
-      sudo subscription-manager unregister || true
-      echo subscription-manager register ... $i
-      sudo subscription-manager register $register_opts && break
-   done
-   for i in {1..10} ; do
-      echo subscription-manager attach ... $i
-      sudo subscription-manager attach $attach_opts && break
-   done
-   set -x
-   for i in {1..10} ; do
-      echo subscription-manager clean repos ... $i
-      sudo subscription-manager repos --disable=* && break
-   done
+cat <<EOF | sudo tee /etc/sysctl.d/10-tf-devstack.conf
+net.ipv6.bindv6only = 0
+net.ipv6.conf.all.forwarding = 1
+net.ipv4.conf.all.forwarding = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system || true
+
+function _register_system() {
+   sudo subscription-manager unregister || true
+   sudo subscription-manager register --username $RHEL_USER --password $RHEL_PASSWORD
+}
+
+if [[ "${ENABLE_RHEL_REGISTRATION}" == 'true' ]] ; then
+   state="$(set +o)"
+   [[ "$-" =~ e ]] && state+="; set -e"
+   set +x
+   echo "subscription-manager register system"
+   wait_cmd_success _register_system 2 5
+   eval "$state"
+  
+   echo "subscription-manager attach ... $i"
+   wait_cmd_success "sudo subscription-manager attach $attach_opts" 2 5
+
+   echo "subscription-manager clean repos ... $i"
+   wait_cmd_success "sudo subscription-manager repos --disable=*" 2 5
+   
    enable_repo_list=''
    for r in $(echo $RHEL_REPOS | tr ',' ' ') ; do enable_repo_list+=" --enable=${r}"; done
-   for i in {1..10} ; do
-      echo subscription-manager repos $enable_repo_list ... $i
-      sudo subscription-manager repos $enable_repo_list && break
-   done
+   echo "subscription-manager repos $enable_repo_list ... $i"
+   wait_cmd_success "sudo subscription-manager repos $enable_repo_list" 2 5
+else
+   sudo subscription-manager config --rhsm.auto_enable_yum_plugins=0
+   sudo subscription-manager config --rhsm.manage_repos=0
 fi
 
-$my_dir/${RHEL_VERSION}_provisioning.sh
+source $my_dir/${RHEL_VERSION}_provisioning.sh
+
+[[ "$ENABLE_TLS" != 'ipa' ]] || sudo update-ca-trust extract
