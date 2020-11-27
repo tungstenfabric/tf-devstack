f350574d991efa024ba028f3e17b0fd7324db0c8
alexey-mr <alexey.morlang@gmail.com>
2020-11-27 16:18:25 +0300

    [rhosp] added ipa support for vexx
    Change-Id: I97fc2732950fa4b24d9ff6b0fbeb4c1818a5dba0

--- rhosp/ipa/freeipa_setup.sh
+++ rhosp/ipa/freeipa_setup.sh
@@ -3,20 +3,23 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-sudo -E $my_dir/freeipa_setup_root.sh
+[ -n "$UndercloudFQDN" ] || {
+    echo "ERROR: UndercloudFQDN env variable must be set"
+    exit 1
+}
 
-domain=$(hostname -d)
+domain=${domain:-$(hostname -d)}
 export CLOUD_DOMAIN_NAME=${CLOUD_DOMAIN_NAME:-"${domain}"}
 
+sudo -E $my_dir/freeipa_setup_root.sh
+
 # Precreate undercloud entry and generate OTP
-otp=$(sudo novajoin-ipa-setup \
+novajoin-ipa-setup \
     --principal admin \
     --password "$AdminPassword" \
     --server `hostname -f` \
     --realm ${CLOUD_DOMAIN_NAME^^} \
     --domain ${CLOUD_DOMAIN_NAME} \
     --hostname ${UndercloudFQDN} \
+    --otp-file ~/undercloud_otp \
     --precreate)
-
-[ -n "$otp" ]
-echo $otp > ~/undercloud_otp
