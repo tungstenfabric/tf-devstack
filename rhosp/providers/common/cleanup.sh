4cbf616f0685281ae3441c0b95dcc0a509abc4f1
alexey-mr <alexey.morlang@gmail.com>
2020-11-27 16:18:25 +0300

    [rhosp] added ipa support for vexx
    Change-Id: I97fc2732950fa4b24d9ff6b0fbeb4c1818a5dba0

--- rhosp/providers/common/cleanup.sh
+++ rhosp/providers/common/cleanup.sh
@@ -1,11 +1,22 @@
 
-source stackrc
+#!/bin/bash
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+cd
+source rhosp-environment.sh
+source $my_dir/../../../common/common.sh
+source $my_dir/../../../common/functions.sh
+source $my_dir/common.sh
+source $my_dir/functions.sh
 
 if [[ "$ENABLE_RHEL_REGISTRATION" == 'true' ]] ; then
   sudo subscription-manager unregister
-  servers=$(openstack server list -c Networks -f value | awk -F '=' '{print $NF}')
+
+  servers=$(get_ctlplane_ips)
   for server in $servers; do
-    ssh -T $ssh_opts heat-admin@${server} "sudo subscription-manager unregister"
+    ssh -T $ssh_opts $SSH_USER_OVERCLOUD@${server} "sudo subscription-manager unregister"
   done
 fi
 
