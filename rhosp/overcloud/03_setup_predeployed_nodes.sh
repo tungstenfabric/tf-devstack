60a843b93d9e94f4017a6e239eecc26b6d7f3299
alexey-mr <alexey.morlang@gmail.com>
2020-11-27 16:18:25 +0300

    [rhosp] added ipa support for vexx
    Change-Id: I97fc2732950fa4b24d9ff6b0fbeb4c1818a5dba0

--- rhosp/overcloud/03_setup_predeployed_nodes.sh
+++ rhosp/overcloud/03_setup_predeployed_nodes.sh
@@ -10,7 +10,8 @@ source $my_dir/../providers/common/common.sh
 
 #Set default gateway to undercloud
 default_dev=$(sudo ip route get $prov_ip | grep -o "dev.*" | awk '{print $2}')
-if [[ -z "$default_dev" || 'lo' == "$default_dev" ]] ; then
+default_ip=$(sudo ip addr show dev $default_dev | awk '/inet /{print($2)}' | cut -d '/' -f 1)
+if [[ -z "$default_dev" || 'lo' == "$default_dev" || -z "$default_ip" ]] ; then
    echo "ERROR: undercloud node is not reachable from overcloud via prov network"
    exit 1
 fi
@@ -19,8 +20,16 @@ cfg_file="/etc/sysconfig/network-scripts/ifcfg-$default_dev"
 sudo ip route replace default via ${prov_ip} dev $default_dev
 sudo sed -i '/^GATEWAY[ ]*=/d' $cfg_file
 echo "GATEWAY=${prov_ip}" | sudo tee -a $cfg_file
+
 sudo sed -i '/nameserver/d'  /etc/resolv.conf
-echo 'nameserver 8.8.8.8' | sudo tee -a /etc/resolv.conf
+if [[ "$ENABLE_TLS" == 'ipa' ]] ; then
+   echo "nameserver $ipa_prov_ip" | sudo tee -a /etc/resolv.conf
+   if ! sudo grep -q "$domain" /etc/resolv.conf ; then
+      sudo sed -i "0,/nameserver/s/\(nameserver.*\)/search ${domain}\n\1/" /etc/resolv.conf
+   fi
+else
+   echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+fi
 
 $my_dir/../providers/common/rhel_provisioning.sh
 
@@ -41,11 +50,66 @@ short_name=$(hostname)
 hosts_names=$fqdn
 [[ "$short_name" != "$short_name" ]] && hosts_names+="    $short_name"
 if ! grep -q "$hosts_names" /etc/hosts ; then
-   default_ip=$(sudo ip addr show dev $default_dev | awk '/inet /{print($2)}' | cut -d '/' -f 1)
    echo "INFO: add resolving $hosts_names to $default_ip"
    [ -n "$default_ip" ] || {
       echo "ERROR: failed to detect ip addr for dev $default_dev"
       exit 1
    }
    echo "$default_ip    $hosts_names" | sudo tee -a /etc/hosts
+fi
+
+if [[ "$ENABLE_TLS" == 'ipa' ]] ; then
+   short_name="$(echo $fqdn | cut -d '.' -f 1)"
+   realm="${domain^^}"
+   novajoin-ipa-setup \
+      --principal admin \
+      --password "$ADMIN_PASSWORD" \
+      --server ${ipa_instance}.${domain} \
+      --realm ${realm} \
+      --domain ${domain} \
+      --hostname ${fqdn} \
+      --otp-file ${fqdn}.otp \
+      --precreate
+
+   sudo hostnamectl set-hostname $fqdn
+   sudo ipa-client-install --verbose -U -w "$(cat ${fqdn}.otp)" --hostname "$fqdn"
+
+   echo "$ADMIN_PASSWORD" | kinit admin
+
+   function _add_node(){
+      local name=$1
+      local zone=$2
+      local addr=$3
+      local services="$4"
+      ipa dnsrecord-add --a-ip-address=$addr ${zone} ${name}
+      ipa host-add ${name}.${zone}
+      local s
+      for s in $services ; do
+         local principal="${s}/${name}.${zone}@${realm}"
+         ipa service-add $principal
+         ipa service-add-host --hosts $fqdn $principal
+      done
+   }
+
+   declare -A networks=( \
+      ['ctlplane']='contrail HTTP' \
+      ['internalapi']='contrail HTTP novnc-proxy redis rabbitmq mysql libvirt qemu' \
+      ['storage']='HTTP' \
+      ['storagemgmt']='HTTP' \
+      ['external']='HTTP' \
+      ['tenant']='contrail' \
+   )
+   # services
+   for net in ${!networks[@]} ; do
+      zone="${net}.${domain}"
+      if ! ipa dnszone-find ${zone} ; then
+         ipa dnszone-add ${zone} || true
+      fi
+      node_ip=$(sudo awk "/$fqdn/{print(\$1)}" /etc/hosts)
+      [ -n "$node_ip" ] || node_ip="$default_ip"
+      _add_node "$short_name" "${zone}" "$node_ip" "${networks[${net}]}"
+      _add_node "overcloud" "${zone}" "$fixed_vip" "haproxy"
+   done
+   # vip public
+   _add_node "overcloud" "$domain" "$fixed_vip" "haproxy"
 fi
--- rhosp/providers/common/rhel7_provisioning.sh
+++ rhosp/providers/common/rhel7_provisioning.sh
@@ -1,16 +1,12 @@
-#!/bin/bash -e
-
 sudo yum update -y
-sudo yum install -y ntp wget yum-utils vim iproute jq curl bind-utils bridge-utils net-tools python-heat-agent*
+
+package="ntp wget yum-utils vim iproute jq curl bind-utils bridge-utils net-tools python-heat-agent* python-virtualenv python-docker python27-python-pip"
+[[ "$ENABLE_TLS" != 'ipa' ]] || packages+=" ipa-client python-novajoin"
+
+sudo yum install -y $package
 
 sudo chkconfig ntpd on
-sudo service ntpd start
-
-# install pip for future run of OS checks
-curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/get-pip.py | sudo python
-sudo pip install -q virtualenv docker
+sudo service ntpd restart
 
 # install pyton3 after pip
-sudo yum install -y python3
-curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/get-pip.py | sudo python3
-sudo python3 -m pip install -q six pyyaml
+sudo yum install -y python3 python3-pyyaml python33-python-six rh-python36-python-six
