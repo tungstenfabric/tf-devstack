e8b67ca3dbf87336c8471de31a7932c60254752a
alexey-mr <alexey.morlang@gmail.com>
2020-11-27 16:18:25 +0300

    [rhosp] added ipa support for vexx
    Change-Id: I97fc2732950fa4b24d9ff6b0fbeb4c1818a5dba0

--- rhosp/undercloud/undercloud_deploy.sh
+++ rhosp/undercloud/undercloud_deploy.sh
@@ -9,19 +9,8 @@ source $my_dir/../../common/common.sh
 source $my_dir/../providers/common/common.sh
 source $my_dir/../providers/common/functions.sh
 
-# ssh config to do not check host keys and avoid garbadge in known hosts files
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-cat <<EOF >~/.ssh/config
-Host *
-StrictHostKeyChecking no
-UserKnownHostsFile=/dev/null
-EOF
-chmod 644 ~/.ssh/config
 
-export local_mtu=`/sbin/ip link show $undercloud_local_interface | grep -o "mtu.*" | awk '{print $2}'`
-
-if [[ -n "$ENABLE_TLS" ]] ; then
+if [[ "$ENABLE_TLS" == 'ipa' ]] ; then
   export undercloud_nameservers="$ipa_prov_ip"
   export nova_join_option="enable_novajoin = True"
   export ipa_otp_option="ipa_otp = \"$OTP_PASSWORD\""
@@ -30,6 +19,8 @@ else
   export nova_join_option=""
   export ipa_otp_option=""
 fi
+
+export local_mtu=`/sbin/ip link show $undercloud_local_interface | grep -o "mtu.*" | awk '{print $2}'`
 
 source $my_dir/${RHOSP_VERSION}_undercloud_deploy.sh
 source $my_dir/${RHOSP_VERSION}_configure_registries_undercloud.sh
