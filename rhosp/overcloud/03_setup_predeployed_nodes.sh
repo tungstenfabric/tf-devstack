a2c4e062f30c017255d5256a1e4eefcf2b990d17
alexey-mr <alexey.morlang@gmail.com>
2020-11-27 16:18:25 +0300

    [rhosp] added ipa support for vexx
    Change-Id: I97fc2732950fa4b24d9ff6b0fbeb4c1818a5dba0

--- rhosp/overcloud/03_setup_predeployed_nodes.sh
+++ rhosp/overcloud/03_setup_predeployed_nodes.sh
@@ -19,8 +19,16 @@ cfg_file="/etc/sysconfig/network-scripts/ifcfg-$default_dev"
 sudo ip route replace default via ${prov_ip} dev $default_dev
 sudo sed -i '/^GATEWAY[ ]*=/d' $cfg_file
 echo "GATEWAY=${prov_ip}" | sudo tee -a $cfg_file
+
 sudo sed -i '/nameserver/d'  /etc/resolv.conf
-echo 'nameserver 8.8.8.8' | sudo tee -a /etc/resolv.conf
+if [[ "$ENABLE_TLS" == 'ipa' ]] ; then
+   echo "nameserver $ipa_prov_ip" | sudo tee -a /etc/resolv.conf
+   if ! sudo grep -q "$domain" /etc/resolv.conf ; then
+      sudo sed -i "0,/nameserver/s/\(nameserver.*\)/search ${domain}\n\1/" /etc/resolv.conf
+   fi
+else
+   echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+fi
 
 $my_dir/../providers/common/rhel_provisioning.sh
 
@@ -48,4 +56,58 @@ if ! grep -q "$hosts_names" /etc/hosts ; then
       exit 1
    }
    echo "$default_ip    $hosts_names" | sudo tee -a /etc/hosts
+fi
+
+if [[ "$ENABLE_TLS" == 'ipa' ]] ; then
+   short_name="$(echo $fqdn | cut -d '.' -f 1)"
+   realm="${domain^^}"
+   novajoin-ipa-setup \
+      --principal admin \
+      --password "$ADMIN_PASSWORD" \
+      --server ${ipa_instance}.${domain} \
+      --realm ${realm} \
+      --domain ${domain} \
+      --hostname ${fqdn} \
+      --otp-file ${fqdn}.otp \
+      --precreate
+
+   sudo hostnamectl set-hostname $fqdn
+   sudo ipa-client-install --verbose -U -w "$(cat ${fqdn}.otp)" --hostname "$fqdn"
+
+   echo "$ADMIN_PASSWORD" | kinit admin
+
+   function _add_node(){
+      local name=$1
+      local zone=$2
+      local addr=$3
+      local services="$4"
+      ipa dnsrecord-add --a-ip-address=$addr ${zone} ${name}
+      ipa host-add ${name}.${zone}
+      local s
+      for s in $services ; do
+         local principal="${s}/${name}.${zone}@${realm}"
+         ipa service-add $principal
+         ipa service-add-host --hosts $fqdn $principal
+      done
+   }
+
+   declare -A networks=( \
+      ['ctlplane']='contrail HTTP' \
+      ['internalapi']='contrail HTTP novnc-proxy redis rabbitmq mysql libvirt qemu' \
+      ['storage']='HTTP' \
+      ['storagemgmt']='HTTP' \
+      ['external']='HTTP' \
+      ['tenant']='contrail' \
+   )
+   # services
+   for net in ${!networks[@]} ; do
+      zone="${net}.${domain}"
+      if ! ipa dnszone-find ${zone} ; then
+         ipa dnszone-add ${zone} || true
+      fi
+      _add_node "$short_name" "${zone}" "$NODE_IP" "${networks[${net}]}"
+      _add_node "overcloud" "${zone}" "$fixed_vip" "haproxy"
+   done
+   # vip public
+   _add_node "overcloud" "$domain" "$fixed_vip" "haproxy"
 fi
