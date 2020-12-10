68c99b56445e18bb2d8a45c39d3f89f64dab2e58
shrike <shrike_@mail.ru>
2020-12-10 13:21:48 +0000

    bump keystone wsgi workers count
    helm-openstack deploys keystone with 1 process, 1 thread. this is
    not enough for handling the load generated by contrail installation,
    keystone is unable to handle token requests and simply drops
    connections.
    let's add MORE worker processes.
    
    related-jira-bug: CEM-19790
    Change-Id: I9fa4a96ca7257dceac54c984312fa6036e0db643

--- helm/deploy_helm_openstack.sh
+++ helm/deploy_helm_openstack.sh
@@ -47,6 +47,34 @@ pod:
           params:
             failureThreshold: 30
             timeoutSeconds: 30
+conf:
+  wsgi_keystone: |
+    {{- $portInt := tuple "identity" "internal" "api" $ | include "helm-toolkit.endpoints.endpoint_port_lookup" }}
+
+    Listen 0.0.0.0:{{ $portInt }}
+
+    LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+    LogFormat "%{X-Forwarded-For}i %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" proxy
+
+    SetEnvIf X-Forwarded-For "^.*\..*\..*\..*" forwarded
+    CustomLog /dev/stdout combined env=!forwarded
+    CustomLog /dev/stdout proxy env=forwarded
+
+    <VirtualHost *:{{ $portInt }}>
+        WSGIDaemonProcess keystone-public processes=53 threads=1 user=keystone group=keystone display-name=%{GROUP}
+        WSGIProcessGroup keystone-public
+        WSGIScriptAlias / /var/www/cgi-bin/keystone/keystone-wsgi-public
+        WSGIApplicationGroup %{GLOBAL}
+        WSGIPassAuthorization On
+        <IfVersion >= 2.4>
+          ErrorLogFormat "%{cu}t %M"
+        </IfVersion>
+        ErrorLog /dev/stdout
+
+        SetEnvIf X-Forwarded-For "^.*\..*\..*\..*" forwarded
+        CustomLog /dev/stdout combined env=!forwarded
+        CustomLog /dev/stdout proxy env=forwarded
+    </VirtualHost>
 ...
 YAML
 
