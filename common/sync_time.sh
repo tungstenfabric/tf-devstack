b60947acedbb9c846f4bd2479061ac4409a75037
alexey-mr <alexey.morlang@gmail.com>
2020-12-23 12:42:39 +0300

    Fixed retries for ntp sync
    - added ntp files to logs
    
    Change-Id: I9ed92e4c5157bb48d2816b1f5c2f4424f2ed422d

--- common/sync_time.sh
+++ common/sync_time.sh
@@ -1,33 +1,54 @@
-#!/bin/bash -e
+#!/bin/bash
 
-if ps ax | grep "bin/chronyd" | grep -v grep ; then
-  echo "INFO: check sync with chronyd"
-  i=12
-  while ! chronyc -n sources | grep "^\^\*" ; do
-    echo "INFO: time is not synced. force it ($i)"
+[[ "$DEBUG" == true ]] && set -x
+
+function chrony_sync() {
+  local out="$(chronyc -n sources)"
+  if ! echo "$out" | grep -q "^\^\*" ; then
+    echo "INFO: time is not synced, force it"
+    echo "$out"
     sudo systemctl stop chronyd.service
-    for server in $(grep "^server " /etc/chrony.conf | awk '{print $2}') ; do
+    local cfg_file='/etc/chrony.conf'
+    [ -e "$cfg_file" ] || cfg_file='/etc/chrony/chrony.conf'
+    local server
+    for server in $(grep "^server " | awk '{print $2}') ; do
       sudo chronyd -q server $server iburst
     done
     sudo systemctl start chronyd.service
-    sleep 10
-    if ! ((i=-1)) ; then
-      echo "ERROR: time can not be synced. Exiting..."
-      exit 1
-    fi
-  done
-elif ps ax | grep "bin/ntpd" | grep -v grep  ; then
-  echo "INFO: check sync with ntpd"
-  i=12
-  while ! /usr/sbin/ntpq -n -c pe | grep "^\*" ; do
-    echo "INFO: time is not synced. force it ($i)"
+    return 1
+  fi
+}
+
+function ntp_sync() {
+  local out="$(/usr/sbin/ntpq -n -c pe)"
+  if ! echo "$out" | grep -q "^\*" ; then
+    echo "INFO: time is not synced, force it"
+    echo "$out"
     sudo systemctl stop ntpd.service
     sudo ntpd -gq
     sudo systemctl start ntpd.service
-    sleep 10
-    if ! ((i=-1)) ; then
-      echo "ERROR: time can not be synced. Exiting..."
-      exit 1
-    fi
-  done
+  fi
+}
+
+if ps ax | grep -v grep | grep -q "bin/chronyd" ; then
+  time_sync_func=chrony_sync
+elif ps ax | grep -v grep | grep -q "bin/ntpd" ; then
+  time_sync_func=ntp_sync
 fi
+
+if [ -z "$time_sync_func" ] ; then
+  echo "ERROR: unknown time sync system"
+  exit 1
+fi
+
+i=12
+while ! $time_sync_func ; do
+  if ! ((i=-1)) ; then
+    echo "ERROR: time can not be synced. Exiting..."
+    exit 1
+  fi
+  sleep 10
+  echo "INFO: time can not be synced, retry $i"
+done
+
+echo "INFO: time is synced"
