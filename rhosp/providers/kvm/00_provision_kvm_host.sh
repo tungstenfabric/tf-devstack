ca1354596e9e0991547a5e31c124caca4d3455ad
Andrey Pavlov <andrey-mp@yandex.ru>
2021-03-08 10:00:01 +0300

    change path for get-pip.py
    Change-Id: I8a38d13db1846a48694740c982f6c27f70157a19

--- rhosp/providers/kvm/00_provision_kvm_host.sh
+++ rhosp/providers/kvm/00_provision_kvm_host.sh
@@ -12,7 +12,7 @@ apt-get update
 apt-get install -y git qemu-kvm iptables-persistent ufw virtinst uuid-runtime \
         qemu-kvm libvirt-clients libguestfs-tools libvirt-daemon-system bridge-utils virt-manager awscli python-dev hugepages gcc
 
-curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/get-pip.py | python
+curl --retry 3 --retry-delay 10 https://bootstrap.pypa.io/pip/get-pip.py | python
 #virtualbmc==2.0.0 doesn't do node introspection in tripleo. 1.5.0 works well!
 pip install virtualbmc==1.5.0 configparser
 
