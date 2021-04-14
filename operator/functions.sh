2f90d06436dfd58b514c4f28191650c844da398a
Arina Argentova <aargentova@progmaticlab.com>
2021-04-14 17:49:39 +0300

    test jenkins passing OS_DATA_NETWORK_CIDR to operator
    Depends-on: I4a979712087368b4e0c345370d77498aeb0daba9
    
    Signed-off-by: aargentova@progmaticlab.com
    Change-Id: Ieb899fe0f20ec2804b98d674d8c122e8ae5042a8

--- operator/functions.sh
+++ operator/functions.sh
@@ -41,3 +41,12 @@ EOF
         popd
     done
 }
+
+function get_vrouter_gateway() {
+    cidr=${DATA_NETWORK}
+    net=$(ipcalc -n "$cidr" | cut -f2 -d=)
+    IFS=. read -r a b c d <<< "$net"
+    d=$(($d+1))
+    vrouter_gateway="$a.$b.$c.$d"
+    echo $vrouter_gateway
+}
