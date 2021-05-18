2c80d83b92322b9b41d039c9e0aa243d02f1d45f
Gleb Galkin <gleb108@gmail.com>
2021-05-18 20:59:52 +0300

    Updated RHEL_POOL_ID
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: Icce4ca28e7a5afa8a17a879a5ebe4b170b38795e

--- rhosp/config/common.sh
+++ rhosp/config/common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export RHEL_POOL_ID=${RHEL_POOL_ID}
-[ -n "$RHEL_POOL_ID" ] || RHEL_POOL_ID='8a85f999759ed5b40175b8b0bdad6329'
+[ -n "$RHEL_POOL_ID" ] || RHEL_POOL_ID='8a85f999759ed5b40175b8b101f4632d'
 
 state="$(set +o)"
 [[ "$-" =~ e ]] && state+="; set -e"
