0a2960aa759f9314e3fdb472e89a8694c1d71a5c
Andrey Pavlov <andrey-mp@yandex.ru>
2021-05-19 07:59:48 +0300

    increase wait timeout for maas
    Change-Id: I7e9138dbe4da2fd7186602053d186ac7e8417780

--- juju/run.sh
+++ juju/run.sh
@@ -29,8 +29,8 @@ export PATH=$PATH:/snap/bin
 
 # default env variables
 export DEPLOYER='juju'
-# max wait in seconds after TF deployment (openstack AIO ~ 1300, k8s AIO ~ 2100, maas HA ~ 2400???3600???4800)
-export WAIT_TIMEOUT=${WAIT_TIMEOUT:-4800}
+# max wait in seconds after TF deployment (openstack AIO ~ 1300, k8s AIO ~ 2100, maas HA ~ 2400???3600???4800??6000)
+export WAIT_TIMEOUT=${WAIT_TIMEOUT:-6000}
 export JUJU_REPO=${JUJU_REPO:-$WORKSPACE/tf-charms}
 export CLOUD=${CLOUD:-manual}  # aws | maas | manual
 # cloud local is deprecated, please use CLOUD=manual and unset CONTROLLER_NODES and AGENT_NODES
