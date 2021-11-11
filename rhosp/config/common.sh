1694dc0661d09aac8ad0e2286062490e5c69eb5e
Gleb Galkin <gleb108@gmail.com>
2021-11-11 15:32:41 +0300

    rhosp: Fix POOL_ID
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I02ca7deb99b69ba8df861c952df2b5eb344ccfef

--- rhosp/config/common.sh
+++ rhosp/config/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -n "$RHEL_POOL_ID" ] || RHEL_POOL_ID='8a85f999759ed5b40175b8b101f4632d'
+[ -n "$RHEL_POOL_ID" ] || RHEL_POOL_ID='8a85f99b77b0c6850177ceb51fdd237b'
 export RHEL_POOL_ID=$RHEL_POOL_ID
 
 state="$(set +o)"
