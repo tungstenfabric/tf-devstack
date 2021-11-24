fc7cc41c6095d2b04b4e5bdd078e83f9ec629c44
Alexey <alexey.morlang@gmail.com>
2021-11-24 21:17:01 +0300

    fixed rhsm release for rhosp16.2 and rhosp16.1
    Change-Id: I4781dcb1f3c4cea92aad2704cc75d52ee677f2d7

--- rhosp/overcloud/rhosp16_prepare_heat_templates.sh
+++ rhosp/overcloud/rhosp16_prepare_heat_templates.sh
@@ -5,5 +5,7 @@ if [[ "$ENABLE_RHEL_REGISTRATION" == 'true' ]] ; then
     sed -i "/rhsm_repos:/ a \ \ \ \ \ \ - $i" $my_dir/rhsm.yaml.template
   done
 
+  declare -A _rhsm_releases=( ["rhosp13"]='7.9' ['rhosp16.1']='8.2' ['rhosp16.2']='8.4' )
+  export RHSM_RELEASE=${_rhsm_releases[$RHOSP_VERSION]}
   cat $my_dir/rhsm.yaml.template | envsubst > rhsm.yaml
 fi
