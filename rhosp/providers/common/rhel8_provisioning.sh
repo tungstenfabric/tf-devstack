9285990387c11887457bc155e159129722e38ba6
Alexey <alexey.morlang@gmail.com>
2021-11-25 18:35:17 +0300

    fixed container-tools module for rhosp16
    Change-Id: Ib43a99b04ec02c0fc34c51c61c3583aecac05142

--- rhosp/providers/common/rhel8_provisioning.sh
+++ rhosp/providers/common/rhel8_provisioning.sh
@@ -2,13 +2,29 @@
 sudo dnf remove -y docker-ce-cli || true
 
 # fix module otherwise upstream usage leads to packages conflicts
-sudo dnf module disable -y container-tools idm
-sudo dnf module enable -y container-tools:2.0 idm:DL1
+echo "INFO: dnf disable modules container-tools idm"
+sudo dnf module disable -y container-tools idm || true
+
+declare -A _dnf_container_tools=(
+  ["rhel8.2"]="container-tools:2.0"
+  ["rhel8.4"]="container-tools:3.0"
+)
+_ctools=${_dnf_container_tools[$RHEL_VERSION]}
+if [ -z "$_ctools" ] ; then
+  echo "ERROR: internal error - no container-tools set for $RHEL_VERSION"
+  exit 1
+fi
+echo "INFO: dnf enable $_ctools"
+sudo dnf module enable -y $_ctools
+
+echo "INFO: dnf enable idm:DL1"
+sudo dnf module enable -y idm:DL1
 
 #Fix for ceph-storage issue https://access.redhat.com/solutions/5912141
-sudo dnf module disable -y virt:rhel
+sudo dnf module disable -y virt:rhel || true
 #rhel_ver=$(echo $RHEL_VERSION | sed "s/rhel//" )
 #sudo dnf module enable -y virt:${rhel_ver}
+echo "INFO: enable virt:8.2"
 sudo dnf module enable -y virt:8.2
 
 sudo dnf distro-sync -y
