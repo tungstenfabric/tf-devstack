b5db07a28c5e5ac01eb63608c60ea0a782871319
Alexey <alexey.morlang@gmail.com>
2022-01-21 11:26:37 +0300

    openshift: fix log and avoid error on destroy bootstrap
    Change-Id: I10e8c687541021f19d5d70983164f1579eb7d84d

--- openshift/providers/kvm/destroy_bootstrap.sh
+++ openshift/providers/kvm/destroy_bootstrap.sh
@@ -6,8 +6,8 @@ my_dir="$(dirname $my_file)"
 source $my_dir/definitions
 
 # Remove bootstrap node
-sudo virsh destroy ${KUBERNETES_CLUSTER_NAME}-bootstrap > /dev/null || err "virsh destroy ${KUBERNETES_CLUSTER_NAME}-bootstrap failed"
-sudo virsh undefine ${KUBERNETES_CLUSTER_NAME}-bootstrap --remove-all-storage > /dev/null || err "virsh undefine ${KUBERNETES_CLUSTER_NAME}-bootstrap --remove-all-storage"
+sudo virsh destroy ${KUBERNETES_CLUSTER_NAME}-bootstrap || echo "INFO: virsh destroy ${KUBERNETES_CLUSTER_NAME}-bootstrap failed"
+sudo virsh undefine ${KUBERNETES_CLUSTER_NAME}-bootstrap --remove-all-storage || echo "INFO: virsh undefine ${KUBERNETES_CLUSTER_NAME}-bootstrap --remove-all-storage failed"
 ssh -i ${OPENSHIFT_SSH_KEY} $SSH_OPTS "${LB_SSH_USER}@lb.${KUBERNETES_CLUSTER_NAME}.${KUBERNETES_CLUSTER_DOMAIN}" \
     "sed -i '/bootstrap\.${KUBERNETES_CLUSTER_NAME}\.${KUBERNETES_CLUSTER_DOMAIN}/d' /etc/haproxy/haproxy.cfg" || err "failed"
 ssh -i ${OPENSHIFT_SSH_KEY} $SSH_OPTS "${LB_SSH_USER}@lb.${KUBERNETES_CLUSTER_NAME}.${KUBERNETES_CLUSTER_DOMAIN}" "systemctl restart haproxy" || err "failed"
