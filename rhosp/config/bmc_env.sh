190232f0b1775b09e4122ec12b6a6fba86e76489
Gleb Galkin <gleb108@gmail.com>
2022-02-25 17:58:16 +0300

    Adding outside variables to bmc_env.sh
    partial-jira-bug: CEM-25816
    partial-jira-bug: CEM-25903
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I88a4e06e0dc1ad4ce1c5ebd4702fe9efa4d2d0e2

--- rhosp/config/bmc_env.sh
+++ rhosp/config/bmc_env.sh
@@ -25,33 +25,33 @@ export undercloud_admin_host="${prov_subnet}.3"
 export undercloud_public_host="${prov_subnet}.4"
 
 # Interfaces for providing tests run (need only if network isolation enabled)
-export internal_vlan="vlan710"
-export internal_interface="eth1"
-export internal_ip_addr="10.1.0.5"
-export internal_net_mask="255.255.255.0"
+export internal_vlan="${internal_vlan:-vlan710}"
+export internal_interface="${internal_interface:-eth1}"
+export internal_ip_addr="10.${DEPLOY_POSTFIX}.1.5"
+export internal_net_mask="${internal_net_mask:-"255.255.255.0"}"
 
-export external_vlan="vlan720"
-export external_interface="eth1"
-export external_ip_addr="10.2.0.5"
-export external_net_mask="255.255.255.0"
+export external_vlan="${external_vlan:-vlan720}"
+export external_interface="${external_interface:-eth1}"
+export external_ip_addr="10.${DEPLOY_POSTFIX}.2.5"
+export external_net_mask="${external_net_mask:-255.255.255.0}"
 
-export tenant_ip_net="10.0.0.0/24"
+export tenant_ip_net="10.${DEPLOY_POSTFIX}.0.0/24"
 
 # TODO: rework after AGENT_NODES, CONTROLLER_NODES be used as an input for rhosp
-export overcloud_cont_instance="1,2,3"
-export overcloud_ctrlcont_instance="1,2,3"
-export overcloud_compute_instance="1"
-export overcloud_dpdk_instance="1"
-export overcloud_sriov_instance="1"
-#export overcloud_ceph_instance="1,2,3"
+export overcloud_cont_instance="${overcloud_cont_instance:-1,2,3}"
+export overcloud_ctrlcont_instance="${overcloud_ctrlcont_instance:-1,2,3}"
+export overcloud_compute_instance="${overcloud_compute_instance:-1}"
+export overcloud_dpdk_instance="${overcloud_dpdk_instance}"
+export overcloud_sriov_instance="${overcloud_sriov_instance}"
+export overcloud_ceph_instance="${overcloud_ceph_instance:-1,2,3}"
 
 # to allow nova to use hp as well (2 are used by vrouter)
-export vrouter_huge_pages_1g='32'
+export vrouter_huge_pages_1g="32"
 
 #SRIOV parameters
-export sriov_physical_interface="ens2f3"
-export sriov_physical_network="sriov1"
-export sriov_vf_number="4"
+export sriov_physical_interface="${sriov_physical_interface:-ens2f3}"
+export sriov_physical_network="${sriov_physical_network:-sriov1}"
+export sriov_vf_number="${sriov_vf_number:-4}"
 
 # IPA params
 export ipa_instance="ipa"
