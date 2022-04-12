befac9e0eb1a602e2424e819bd58ad72806de750
Gleb Galkin <gleb108@gmail.com>
2022-04-12 14:51:48 +0300

    Fix sriov and dpdk network for openlab1
    partial-jira-bug: CEM-25816
    partial-jira-bug: CEM-25905
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I3e9612ef2bb39079adc21422b269ef3ebce97fb0

--- rhosp/overcloud/rhosp13_prepare_heat_templates.sh
+++ rhosp/overcloud/rhosp13_prepare_heat_templates.sh
@@ -4,3 +4,19 @@ if [[ "$ENABLE_RHEL_REGISTRATION" == 'true' ]] ; then
 fi
 
 cat $my_dir/firstboot_userdata.yaml.template | envsubst > firstboot_userdata.yaml
+
+if [[ -n "$overcloud_dpdk_instance" ]]; then
+  #Changing network interfaces for baremetal dpdk node in openlab1
+  echo "INFO Changing network template for openlab1 contrail-dpdk-nic-config-single.yaml"
+  sed -i "s/nic1/nic3/" tripleo-heat-templates/network/config/contrail/contrail-dpdk-nic-config-single.yaml
+  sed -i "s/nic2/nic4/" tripleo-heat-templates/network/config/contrail/contrail-dpdk-nic-config-single.yaml
+fi
+
+if [[ -n "$overcloud_sriov_instance" ]]; then
+  #Changing network interfaces for baremetal sriov node in openlab1
+  echo "INFO Changing network template for openlab1 contrail-sriov-nic-config.yaml"
+  sed -i "s/nic1/nic3/" tripleo-heat-templates/network/config/contrail/contrail-sriov-nic-config.yaml
+  sed -i "s/nic2/nic4/" tripleo-heat-templates/network/config/contrail/contrail-sriov-nic-config.yaml
+fi
+
+
