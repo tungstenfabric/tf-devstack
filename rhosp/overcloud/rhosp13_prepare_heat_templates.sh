fc9425b2b81d49a00cc3e832e8ac6b0ea7e2b903
Gleb Galkin <gleb108@gmail.com>
2022-04-18 12:28:40 +0300

    rhosp13: added patch for Red Hat ipaclient.yaml
    partial-jira-bug: CEM-25816
    partial-jira-bug: CEM-25905
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: Ic980b306fdfaffe8883fec9a7214554eed127a47

--- rhosp/overcloud/rhosp13_prepare_heat_templates.sh
+++ rhosp/overcloud/rhosp13_prepare_heat_templates.sh
@@ -7,7 +7,7 @@ cat $my_dir/firstboot_userdata.yaml.template | envsubst > firstboot_userdata.yam
 
 if [[ -n "$overcloud_dpdk_instance" ]]; then
   #Changing network interfaces for baremetal dpdk node in openlab1
-  echo "INFO Changing network template for openlab1 contrail-dpdk-nic-config-single.yaml"
+  echo "INFO: Changing network template for openlab1 contrail-dpdk-nic-config-single.yaml"
   sed -i "s/nic1/nic3/" tripleo-heat-templates/network/config/contrail/contrail-dpdk-nic-config-single.yaml
   sed -i "s/nic2/nic4/" tripleo-heat-templates/network/config/contrail/contrail-dpdk-nic-config-single.yaml
 fi
@@ -19,4 +19,11 @@ if [[ -n "$overcloud_sriov_instance" ]]; then
   sed -i "s/nic2/nic4/" tripleo-heat-templates/network/config/contrail/contrail-sriov-nic-config.yaml
 fi
 
+#Patch ipaclient.yaml for checking /etc/resolv.conf (sometimes ipa dns server disappears from resolv.conf. Race condition or something)
+if [[ "$ENABLE_TLS" == 'ipa' ]] ; then
+  pushd tripleo-heat-templates/extraconfig/services/
+  echo "INFO: Changing extraconfig/services/ipaclient.yaml for preventing resolv.conf issue"
+  patch ipaclient.yaml $my_dir/ipaclient.patch
+  popd
+fi
 
