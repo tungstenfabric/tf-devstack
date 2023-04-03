260ed895ce9df52858c0f16c4b8057fad5071035
Gleb Galkin <gleb108@gmail.com>
2023-04-03 19:57:06 +0400

    Updating R~ed Hat POOL_ID
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: Ie7e702463235394f1a668361073887c4ed2d3fb7

--- rhosp/config/common.sh
+++ rhosp/config/common.sh
@@ -6,7 +6,7 @@
 
 # for 'physical' type we have to define another pool id
 
-[ -n "$RHEL_POOL_ID" ] || RHEL_POOL_ID='8a85f99b7ed9423e017ed9f30a062766'
+[ -n "$RHEL_POOL_ID" ] || RHEL_POOL_ID='2c948b68859c15590185badd1e292e03'
 export RHEL_POOL_ID=$RHEL_POOL_ID
 
 state="$(set +o)"
